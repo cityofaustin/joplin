808a272521db52cee1584bb3c5225d29b4b7ea61
Simi <Simi.Damani@austintexas.gov>
2017-11-30 15:10:22 -0600

    cors config to access api data on local janis

--- joplin/settings.py
+++ joplin/settings.py
@@ -52,10 +52,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'wagtail.contrib.modeladmin',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -68,6 +70,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
