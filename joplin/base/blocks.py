daa657507f8f2ed3e6ed51a2a96c8dc99be79525
Ben <benweatherman@gmail.com>
2018-01-19 15:44:19 -0600

    Add Recollect and map blocks
    Por ejemplo
    
    ![add map and recollect blocks](https://user-images.githubusercontent.com/272675/35172997-7574652c-fd2f-11e7-8c44-1b8dd7cb9188.gif)
    
    Example graphql response for page with a map block
    
    ```
    {
      "data": {
        "servicePage": {
          "content": "<p></p><ol><li>Visit the ReUse Store in the Recycle &amp; Reuse Drop-Off Center at 2514 Business Center Dr, Austin, TX 78744.<br/></li><li>Pick from the available 3 colors of Austin ReBlend paint. The paint is free for all residents.<br/></li></ol><p></p>",
          "extraContent": [
            {
              "id": "d2998f46-3c68-480e-af20-38c7fce43e82",
              "type": "map_block",
              "value": {
                "description": "Recycle and Reuse Drop-off Center",
                "location": {
                  "city": "Austin",
                  "country": "United States",
                  "name": "Austin Recycle and Reuse Drop-off Center",
                  "pk": 1,
                  "state": "TX",
                  "street": "2514 Business Center Dr",
                  "zip": "78744"
                },
                "pk": 1
              }
            },
            {
              "id": "fff2155d-fc2b-411d-8c3a-1dcef2fff99c",
              "type": "content",
              "value": "<h2>Learn about Austin ReBlend Paint</h2><p>Austin ReBlend is a free, 100 percent flat paint made from paint dropped off at the Recycle &amp; Reuse Drop-Off Center. The paint is inspected to ensure quality, reblended into three colors and offered in 3.5 gallon containers. The available colors include:</p><ul><li><p>Texas Limestone (beige)</p></li><li><p>Balcones Canyonland (dark beige)</p></li><li><p>Barton Creek Greenbelt (dark green)</p></li></ul><h2>Pick up other free household items</h2><p>Many reusable materials are dropped off at the center. You can also pick up these items for free (availability varies):<br/></p><p></p><ul><li>Art supplies</li><li>Cleaning products</li><li>Household chemicals</li><li>Automotive fluids</li><li>Mulch*</li></ul><p>*You must load mulch yourself. Bring a pitchfork or shovel, work gloves, and containers.</p><p></p>"
            }
          ],
          "id": "U2VydmljZVBhZ2VOb2RlOjU="
        }
      }
    }
    ```
    
    and collection schedule block
    
    ```
    {
      "data": {
        "servicePage": {
          "content": "<ol><li><p>Type your street address in the box below. If you live in a multi-family home, like a duplex or fourplex, do not include your unit number.<br/></p></li><li><p>Press the search button to view your curbside pickup days.<br/></p></li><li><p>Optional: Print a copy of your schedule or click the icon to add your pickup days to your Google, iCal, or Microsoft calendar.<br/></p></li></ol>",
          "extraContent": [
            {
              "id": "5369029b-3100-4226-ae11-2127fe1c3c30",
              "type": "collection_schedule_block",
              "value": null
            },
            {
              "id": "975a8966-601c-4940-a306-4a53ff01b85b",
              "type": "content",
              "value": "<h2>Trash, recycling, and compost pickup</h2><br/><p>Every week, we will pick up your trash, compost, and yard trimmings. Every other week, we will pick up recycling, clothing, and housewares. All curbside pick-ups happen on the same day of the week unless it is a holiday. </p><h2>Holiday pickup days</h2><br/>If your curbside pickup day is on Thanksgiving Day, Christmas Day, or New Year's Day—or if it is after the holiday but in the same week—your collection will be one day later than your usual pickup day that week. If the holiday falls on a Sunday or Saturday, your pickup day will not change.<p></p>"
            }
          ],
          "id": "U2VydmljZVBhZ2VOb2RlOjY="
        }
      }
    }
    ```

--- joplin/base/blocks.py
+++ joplin/base/blocks.py
@@ -1,6 +1,21 @@
+from wagtail.wagtailcore import blocks
 from wagtail.wagtailsnippets.blocks import SnippetChooserBlock
 
 
 class SnippetChooserBlockWithAPIGoodness(SnippetChooserBlock):
     def get_api_representation(self, model_instance, context=None):
         return model_instance.serializable_data()
+
+
+class WhatDoIDoWithBlock(blocks.StaticBlock):
+    class Meta:
+        icon = 'bin'
+        label = 'What do I do with...'
+        admin_text = label
+
+
+class CollectionScheduleBlock(blocks.StaticBlock):
+    class Meta:
+        icon = 'bin'
+        label = 'Collection Schedule'
+        admin_text = label
