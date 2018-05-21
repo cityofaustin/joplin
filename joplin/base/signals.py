55261c475f8d76be71f7700c2576680662dd4660
Brian Smith <briaguya@gmail.com>
2018-05-21 12:08:55 -0500

    generate a few different image widths

--- joplin/base/signals.py
+++ joplin/base/signals.py
@@ -1,23 +1,12 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from base.models import TranslatedImage
-from django.urls import reverse
-from wagtail.images.views.serve import generate_signature
 
 @receiver(post_save, sender=TranslatedImage)
-def generate_responsive_urls(sender, **kwargs):
-    print("SAVE HAPPENED!")
-    print(sender)
+def generate_responsive_images(sender, **kwargs):
     image = kwargs['instance']
-    url = image.get_rendition('width-320').url
-    print(url)
-    # generate_image_url(image, 'width-320')
-
-def generate_image_url(image, filter_spec):
-    signature = generate_signature(image.id, filter_spec)
-    url = reverse('wagtailimages_serve', args=(signature, image.id, filter_spec))
-
-    # Append image's original filename to the URL (optional)
-    url += image.file.name[len('original_images/'):]
-
-    return url
+    image.get_rendition('width-320') #iPhone 5/SE
+    image.get_rendition('width-375') #iPhone 6/7/8/X
+    image.get_rendition('width-414') #iPhone 6/7/8 Plus
+    # TODO: get_rendition for other widths we want
+    # TODO: retina resultions
