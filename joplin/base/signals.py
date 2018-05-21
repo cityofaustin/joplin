f09ac79f609b8ff5d6e2075538c82672570973f5
Brian Smith <briaguya@gmail.com>
2018-05-21 13:53:51 -0500

    get the original image into /images so we don't need to use the /original_images folder

--- joplin/base/signals.py
+++ joplin/base/signals.py
@@ -5,6 +5,7 @@ from base.models import TranslatedImage
 @receiver(post_save, sender=TranslatedImage)
 def generate_responsive_images(sender, **kwargs):
     image = kwargs['instance']
+    image.get_rendition('original')
     image.get_rendition('width-320') #iPhone 5/SE
     image.get_rendition('width-375') #iPhone 6/7/8/X
     image.get_rendition('width-414') #iPhone 6/7/8 Plus
