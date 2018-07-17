251fba152b726245213b2b6964b05038ad4f1dbc
Brian Smith <briaguya@gmail.com>
2018-07-17 01:28:04 -0500

    progress on the share link working

--- joplin/base/templatetags/joplin_tags.py
+++ joplin/base/templatetags/joplin_tags.py
@@ -1,8 +1,11 @@
 from django import template
+import graphene
 
 register = template.Library()
 
 @register.simple_tag
 def global_id(*args, **kwargs):
     revision = kwargs['revision']
-    return revision
+    revision_id = revision.id
+    global_id = graphene.Node.to_global_id('PageRevisionNode', revision_id)
+    return global_id
