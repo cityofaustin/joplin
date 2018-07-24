7ae6bbaf71a871ae781aeafe3374c11bbb89e64c
Brian Smith <briaguya@gmail.com>
2018-07-24 00:41:04 -0500

    little js magic for title anchor link

--- joplin/static/js/admin.js
+++ joplin/static/js/admin.js
@@ -1,12 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
   // HACK: I can't find a way to override this in python
-  const title = document.querySelector('label[for=id_title]');
+  const title = document.querySelector("label[for=id_title]");
   if (title) {
-    title.textContent = 'Actionable Title';
+    debugger;
+    title.textContent = "";
+    titleLink = document.createElement("a");
+    titleText = document.createTextNode("Actionable Title");
+    titleLink.appendChild(titleText);
+    titleLink.setAttribute(
+      "href",
+      "https://briaguya.github.io/digital-services-style-guide/writing-service-pages/#title"
+    );
+    titleLink.setAttribute("target", "sidebar-iframe");
+    title.appendChild(titleLink);
   }
 
   // HACK: I can't find a way to configure this to be closed via python
-  document.querySelectorAll('#extra_content-prependmenu:not(.stream-menu-closed)').forEach((elem) => {
-    elem.click();
-  });
+  document
+    .querySelectorAll("#extra_content-prependmenu:not(.stream-menu-closed)")
+    .forEach(elem => {
+      elem.click();
+    });
 });
