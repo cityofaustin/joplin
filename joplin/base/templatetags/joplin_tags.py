669e72469c423a608ca54bffd597ec1f0644d8a8
Brian Smith <briaguya@gmail.com>
2018-07-26 14:32:40 -0500

    style guide urls for process page

--- joplin/base/templatetags/joplin_tags.py
+++ joplin/base/templatetags/joplin_tags.py
@@ -16,8 +16,16 @@ def get_preview_url(*args, **kwargs):
     if "Process" in page_type:
       url_page_type = "processes"
 
-    print(page_type)
-
     global_id = graphene.Node.to_global_id('PageRevisionNode', revision.id)
     # TODO: Add other languages
     return os.environ["JANIS_URL"] + "/en/preview/" + url_page_type + "/" + global_id
+
+@register.simple_tag
+def get_style_guide_url(*args, **kwargs):
+  content_type = kwargs['content_type'].name
+
+  if "service" in content_type:
+    return "https://briaguya.github.io/digital-services-style-guide/writing-service-pages/"
+
+  if "process" in content_type:
+    return "https://briaguya.github.io/digital-services-style-guide/writing-process-pages/"
--- joplin/static/js/admin.js
+++ joplin/static/js/admin.js
@@ -1,21 +1,19 @@
 // TODO: This a better way
-const anchorLinks = {
-  id_title:
-    "https://briaguya.github.io/digital-services-style-guide/writing-service-pages/#title",
-  id_steps:
-    "https://briaguya.github.io/digital-services-style-guide/writing-service-pages/#steps",
-  id_apps:
-    "https://briaguya.github.io/digital-services-style-guide/writing-service-pages/#apps",
-  id_additional_content:
-    "https://briaguya.github.io/digital-services-style-guide/writing-service-pages/#additional",
-  id_contacts:
-    "https://briaguya.github.io/digital-services-style-guide/writing-service-pages/#contacts"
+const anchors = {
+  id_title: "#title",
+  id_steps: "#steps",
+  id_apps: "#apps",
+  id_additional_content: "#additional",
+  id_contacts: "#contacts",
+  id_description: "#description",
+  "id_process_steps-0-title": "#step-title"
 };
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // HACK: I can't find a way to override this in python
   // Get all labels and turn them into links
   const labels = document.querySelectorAll("label");
+  const styleGuideUrl = document.getElementById("style_guide_url").value;
 
   for (const label of labels) {
     let id = label.getAttribute("for");
@@ -28,10 +26,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
         case "CONTACTS":
           id = "id_contacts";
           break;
+        case "PROCESS STEPS":
+          id = "id_steps";
+          break;
       }
     }
 
-    if (id in anchorLinks) {
+    if (id in anchors) {
       text = document.createTextNode(label.textContent);
       label.textContent = "";
       if (id === "id_title") {
@@ -39,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
       link = document.createElement("a");
       link.appendChild(text);
-      link.setAttribute("href", anchorLinks[id]);
+      link.setAttribute("href", `${styleGuideUrl}/${anchors[id]}`);
       link.setAttribute("target", "sidebar-iframe");
       label.appendChild(link);
     }
