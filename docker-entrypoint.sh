7f4c97202c0178984dca05da24a806d700111b21
Brian Smith <briaguya@gmail.com>
2018-08-13 18:49:32 -0500

    move load data to it's own shell script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,22 +4,7 @@ set -o errexit
 echo "Running migrations..."
 python ./joplin/manage.py migrate --noinput
 
-if [ "x$LOAD_DATA" = 'xon' ]; then
-  echo "Loading users..."
-  python ./joplin/manage.py loaddata fixtures/users.json
-
-  echo "Loading data..."
-  python ./joplin/manage.py loadcontent \
-        fixtures/images.yaml \
-        fixtures/311.yaml \
-        fixtures/themes.yaml \
-        fixtures/topics.yaml \
-        fixtures/locations.yaml \
-        fixtures/contacts.yaml \
-        fixtures/departments.yaml \
-        fixtures/services \
-        fixtures/processes
-fi
+./load-data.sh
 
 if [ "x$WATCH_LOCAL" = 'xon' ]; then
   echo "Watching using the assets container. Skipping webpack step."
