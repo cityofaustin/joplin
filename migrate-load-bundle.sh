6c76312dfbdf2d9f5c8981d36afa48395cf99f82
Brian Smith <briaguya@gmail.com>
2018-08-13 21:06:08 -0500

    try getting rid of conditions

--- migrate-load-bundle.sh
+++ migrate-load-bundle.sh
@@ -1,22 +1,20 @@
 echo "Running migrations..."
 python ./joplin/manage.py migrate --noinput
 
-if [ "x$LOAD_DATA" = 'xon' ]; then
-  echo "Loading users..."
-  python ./joplin/manage.py loaddata fixtures/users.json
+echo "Loading users..."
+python ./joplin/manage.py loaddata fixtures/users.json
 
-  echo "Loading data..."
-  python ./joplin/manage.py loadcontent \
-        fixtures/images.yaml \
-        fixtures/311.yaml \
-        fixtures/themes.yaml \
-        fixtures/topics.yaml \
-        fixtures/locations.yaml \
-        fixtures/contacts.yaml \
-        fixtures/departments.yaml \
-        fixtures/services \
-        fixtures/processes
-fi
+echo "Loading data..."
+python ./joplin/manage.py loadcontent \
+      fixtures/images.yaml \
+      fixtures/311.yaml \
+      fixtures/themes.yaml \
+      fixtures/topics.yaml \
+      fixtures/locations.yaml \
+      fixtures/contacts.yaml \
+      fixtures/departments.yaml \
+      fixtures/services \
+      fixtures/processes
 
 if [ "x$LOCAL" = 'xon' ]; then
   echo "Watching using the assets container. Skipping webpack step."
