c4c11e76c80bd57c13b4b31729ead0a90c433f7f
Brian Smith <briaguya@gmail.com>
2018-08-14 22:34:10 -0500

    move some code out into js file

--- joplin/js/editor.js
+++ joplin/js/editor.js
@@ -27,4 +27,66 @@ $(function() {
 
     $button.click();
   });
+
+  function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      var successful = document.execCommand("copy");
+      var msg = successful ? "successful" : "unsuccessful";
+      console.log("Fallback: Copying text command was " + msg);
+    } catch (err) {
+      console.error("Fallback: Oops, unable to copy", err);
+    }
+
+    document.body.removeChild(textArea);
+  }
+
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(
+      function() {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function(err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  }
+
+  var editform = $("#page-edit-form");
+  var previewbutton = $("#page-preview-button");
+  var sharebutton = $("#page-share-preview-button");
+  var urlcopied = $("#page-share-url-copied");
+  var messages = $(".messages");
+
+  if (localStorage.previewing === "true") {
+    window.open("{{preview_url}}", "_blank");
+    localStorage.previewing = false;
+  }
+
+  if (localStorage.sharingpreview === "true") {
+    // TODO: Don't just alert with the preview URL
+    copyTextToClipboard("{{preview_url}}");
+    urlcopied.removeClass("hidden");
+    urlcopied.fadeOut(5000);
+    localStorage.sharingpreview = false;
+  }
+
+  previewbutton.click(function() {
+    localStorage.previewing = true;
+  });
+
+  sharebutton.click(function() {
+    localStorage.sharingpreview = true;
+  });
+
+  messages.fadeOut(5000);
 });
