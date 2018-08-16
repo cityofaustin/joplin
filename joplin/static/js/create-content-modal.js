888de62413ddb4e01bb3204f8468ad7c73d02a6e
Mateo Clarke <code@mateoclarke.com>
2018-08-16 15:21:20 -0500

    wip on 2nd active page

--- joplin/static/js/create-content-modal.js
+++ joplin/static/js/create-content-modal.js
@@ -10,17 +10,45 @@ $(document).ready(function() {
 
   var contentWizardModule = {
     init: function() {
-      console.log('setup contentWizardModule...');
       this.registerEventHandlers();
     },
     registerEventHandlers: function() {
       var thiz = this;
-      console.log(this);
 
       $('.js-page-select').click( function() {
         contentWizardState.type = $(this).data('contentType');
         contentWizardState.redirectUrl = $(this).data('redirecturl');
         thiz.incrementActiveStep();
+        $('.js-page-type').text($(this).data('contentType'));
+
+        $('.js-hideable-fields').each( function (){
+
+          // if department is active & field is show for department
+          //   remove hidden
+          // if department in not active  & field is show for department
+          //   add hidden
+          // if department is not active and field is not show for department
+          //   remove hidden
+          //
+          console.log($(this))
+          console.log('isDeptActive', contentWizardState.type === 'department')
+          console.log('show-', $(this).data('showForDepartment') === "true")
+
+          debugger
+
+          if (contentWizardState.type === 'department' && $(this).data('showForDepartment') === 'true') {
+            debugger
+            $(this).removeClass('content-modal__hidden')
+          } else if (contentWizardState.type === 'department') {
+            $(this).addClass('content-modal__hidden')
+          }
+          // else if (contentWizardState.type !== 'department' && $(this).data('showForDepartment') === 'false') {
+          //   $(this).addClass('content-modal__hidden')
+          // } else if (contentWizardState.type !== 'department' && $(this).data('showForDepartment') === 'false') {
+          //   $(this).removeClass('content-modal__hidden')
+          // }
+        })
+
         console.log(contentWizardState);
       });
 
@@ -28,7 +56,6 @@ $(document).ready(function() {
         contentWizardState.topic = $('.js-topic-item:checked[name=topic]').val();
         console.log(contentWizardState);
         thiz.writeToLocalStorage();
-        debugger
         window.location.href = contentWizardState.redirectUrl;
       });
 
@@ -46,33 +73,43 @@ $(document).ready(function() {
       $('.js-continue').click( function(e) {
         thiz.incrementActiveStep();
       });
-
     },
-    selectClickHandler: function(thing) {
+    focusInput: function() {
+      if (contentWizardState.activeStep === 1) {
+        $('#page-title').get(0).focus();
+      }
+    },
+    toggleButtonRowVisibility: function () {
+      if (contentWizardState.activeStep === 0) {
+        $('#js-content-row').addClass('content-modal__button-row--hidden');
+        $('#js-content-row').removeClass('content-modal__button-row');
+      } else {
+        $('#js-content-row').removeClass('content-modal__button-row--hidden');
+        $('#js-content-row').addClass('content-modal__button-row');
+      }
+    },
+    toggleActiveStep: function () {
+      $('.js-wizard-step').each( function(i) {
+        if (contentWizardState.activeStep === i) {
+          $(this).addClass('content-modal__step--active');
+          $(this).removeClass('content-modal__step--inactive');
+        } else {
+          $(this).removeClass('content-modal__step--active');
+          $(this).addClass('content-modal__step--inactive');
+        }
+      });
     },
     incrementActiveStep: function(){
       contentWizardState.activeStep++;
-      $('.js-wizard-step').each( function(i) {
-        if (contentWizardState.activeStep === i) {
-          $(this).addClass('content-modal__step--active');
-          $(this).removeClass('content-modal__step--inactive');
-        } else {
-          $(this).removeClass('content-modal__step--active');
-          $(this).addClass('content-modal__step--inactive');
-        }
-      });
+      this.toggleButtonRowVisibility();
+      this.focusInput();
+      this.toggleActiveStep();
     },
     decrementActiveStep: function(){
-      contentWizardState.activeStep--
-      $('.js-wizard-step').each( function(i) {
-        if (contentWizardState.activeStep === i) {
-          $(this).addClass('content-modal__step--active');
-          $(this).removeClass('content-modal__step--inactive');
-        } else {
-          $(this).removeClass('content-modal__step--active');
-          $(this).addClass('content-modal__step--inactive');
-        }
-      });
+      contentWizardState.activeStep--;
+      this.toggleButtonRowVisibility();
+      this.focusInput();
+      this.toggleActiveStep();
     },
     writeToLocalStorage: function() {
       localStorage.wagtailCreateModal = JSON.stringify(contentWizardState)
