37b99965de8a2a516311d6b5a9bee528ab30ed97
Mateo Clarke <code@mateoclarke.com>
2018-08-22 02:21:13 -0500

    populate department snippet page

---
+++ joplin/js/insertWizardData.js
@@ -0,0 +1,17 @@
+export default function (){
+
+  console.log('insertWizardData')
+
+    // Populate create new page fields when a user comes from the wizard
+    if(localStorage.wagtailCreateModal) {
+      const modalState = JSON.parse(localStorage.wagtailCreateModal);
+
+      $("#id_topic").val(modalState.topic);
+      if (modalState.type === "department") {
+        $("#id_name").val(modalState.title);
+      } else {
+        $("#id_title").val(modalState.title);
+      }
+      localStorage.removeItem('wagtailCreateModal');
+    }
+};
