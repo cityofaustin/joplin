b405ac1beee404d15bc71d4159906cdd56f76cb2
Mateo Clarke <code@mateoclarke.com>
2018-08-22 15:15:44 -0500

    make webpack config reusable

--- joplin/webpack.build.js
+++ joplin/webpack.build.js
@@ -1,42 +1,6 @@
-var BundleTracker = require("webpack-bundle-tracker");
-var path = require("path");
+var merge = require('webpack-merge');
+var baseConfig = require("./webpack.base.js");
 
-module.exports = {
-  mode: "production",
-  entry: {
-    admin: "./js/admin.js",
-    editor: "./js/editor.js",
-    createContentModal: './js/CreateContentModal.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader" // creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader" // translates CSS into CommonJS
-          },
-          {
-            loader: "sass-loader" // compiles Sass to CSS
-          },
-          {
-            loader: 'babel-loader', // gives us JSX for React
-          }
-        ]
-      }
-    ]
-  },
-  output: {
-    path: path.resolve("./static/webpack_bundles/"),
-    filename: "[name]-[hash].js"
-  },
-  plugins: [
-    new BundleTracker({
-      path: __dirname,
-      filename: "./static/webpack-stats.json"
-    })
-  ]
-};
+module.exports = merge(baseConfig, {
+  mode: "production"
+});
--- joplin/webpack.dev.js
+++ joplin/webpack.dev.js
@@ -1,56 +1,10 @@
-var BundleTracker = require("webpack-bundle-tracker");
-var path = require("path");
+var merge = require('webpack-merge');
+var baseConfig = require("./webpack.base.js");
 
-module.exports = {
+module.exports = merge(baseConfig, {
   mode: "development",
   watch: true,
   watchOptions: {
     poll: true
   },
-  entry: {
-    admin: "./js/admin.js",
-    editor: "./js/editor.js",
-    createContentModal: './js/CreateContentModal.js'
-  },
-  module: {
-    rules: [
-        {
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-              // presets: ['react', 'es2015', 'react-hmre'],
-              plugins: ['transform-class-properties']
-            }
-        },
-        {
-            test: /\.scss$/,
-            loaders: [
-              "style-loader", // creates style nodes from JS strings
-              "css-loader", // translates CSS into CommonJS
-              "sass-loader" // compiles Sass to CSS
-            ]
-        },
-        {
-            test: /\.(png|jp(e*)g|svg)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 8000, // Convert images < 8kb to base64 strings
-                    name: 'images/[hash]-[name].[ext]'
-                }
-            }]
-        }
-    ],
-  },
-  output: {
-    path: path.resolve("./static/webpack_bundles/"),
-    filename: "[name]-[hash].js"
-  },
-  plugins: [
-    new BundleTracker({
-      path: __dirname,
-      filename: "./static/webpack-stats.json"
-    })
-  ]
-};
+});
