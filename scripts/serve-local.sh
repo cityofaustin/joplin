3368d26bb600c18be15d151476090bf1b8de30e4
Sergio <sergiogcx@gmail.com>
2018-08-27 13:53:50 -0500

    Sergio's PR Test

--- scripts/serve-local.sh
+++ scripts/serve-local.sh
@@ -20,7 +20,7 @@ fi
 
 # Get the heroku key. We eat stderr because the heroku cli will warn us that these tokens
 # are short-lived. That's OK in our case because we're just running this locally.
-HEROKU_KEY=$(heroku auth:token 2> /dev/null)
+#HEROKU_KEY=$(heroku auth:token 2> /dev/null)
 
 if [ "$REBUILD" == "on" ]; then
     docker build --no-cache -f Dockerfile.base -t joplin-base .
