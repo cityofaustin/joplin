5448f04499d6cb4e5c9805f1c0b14df71db34bb9
Sergio <sergiogcx@gmail.com>
2018-09-06 02:18:04 -0500

    Skipping collectstatic process, heroku enforces a 60 second boot time.

--- docker-entrypoint-prod.sh
+++ docker-entrypoint-prod.sh
@@ -5,4 +5,7 @@ echo "Bucket User: ${AWS_S3_USER}"
 echo "Bucket ID: ${AWS_S3_KEYID}"...
 
 python ./joplin/manage.py migrate --noinput
-python ./joplin/manage.py collectstatic --noinput -i admin
+
+# Skipping static file deployments for now.
+# Heroku enforces a maximum of 60 seconds for boot time
+# python ./joplin/manage.py collectstatic --noinput
