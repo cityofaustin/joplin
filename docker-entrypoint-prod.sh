7cafb0448f32f8cd2f8fe9c7d84d86da2e0e8450
Sergio <sergiogcx@gmail.com>
2018-09-06 02:13:57 -0500

    Ignoring admin directory

--- docker-entrypoint-prod.sh
+++ docker-entrypoint-prod.sh
@@ -5,4 +5,4 @@ echo "Bucket User: ${AWS_S3_USER}"
 echo "Bucket ID: ${AWS_S3_KEYID}"...
 
 python ./joplin/manage.py migrate --noinput
-python ./joplin/manage.py collectstatic --noinput
+python ./joplin/manage.py collectstatic --noinput -i admin
