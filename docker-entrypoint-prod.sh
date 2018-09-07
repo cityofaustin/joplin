f5a71cc468fcbfb2dd2bc109edd93d2cb13066f8
Sergio <sergiogcx@gmail.com>
2018-09-07 11:18:48 -0500

    Playing with heroku.yml (added back migrate-load-data.sh)

--- docker-entrypoint-prod.sh
+++ docker-entrypoint-prod.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 echo "-- Entrypoint Executed (docker-entrypoint-prod.sh)"
-echo "Bucket: ${AWS_S3_BUCKET}"
-echo "Bucket User: ${AWS_S3_USER}"
-echo "Bucket ID: ${AWS_S3_KEYID}"...
-
-# python ./joplin/manage.py migrate --noinput
-
-# Skipping static file deployments for now.
-# Heroku enforces a maximum of 60 seconds for boot time
-# This process can be done from a Dyno shell
-# python ./joplin/manage.py collectstatic --noinput
-
-
+echo "--    Bucket: ${AWS_S3_BUCKET}"
+echo "--    Bucket User: ${AWS_S3_USER}"
+echo "--    Bucket ID: ${AWS_S3_KEYID}"
 exec "$@"
