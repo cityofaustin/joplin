5d4584c52455461de965e4fe235c7ed40a8df28a
Sergio <sergiogcx@gmail.com>
2018-09-13 18:21:42 -0500

    Trying getting image details from registry

--- .travis/heroku-helper.sh
+++ .travis/heroku-helper.sh
@@ -14,3 +14,16 @@ function heroku_release() {
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
   -H "Authorization: Bearer ${HEROKU_API_KEY}"
 }
+
+
+function heroku_release_beta() {
+
+    imageId=$(docker inspect registry.heroku.com/$1/web --format={{.Id}})
+    payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
+
+    curl -n -X PATCH https://api.heroku.com/apps/$1/formation \
+    -d "$payload" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+    -H "Authorization: Bearer ${HEROKU_API_KEY}"
+}
