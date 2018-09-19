bdd2d976e60018e277fe0dbe6ad655d84dccaefa
Sergio <sergiogcx@gmail.com>
2018-09-19 13:51:14 -0500

    Adding Staging deployment detection

--- migrate-load-data.sh
+++ migrate-load-data.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Running migrations..."
+echo "Running migrations: (${DEPLOYMENT_MODE}) ..."
 python ./joplin/manage.py migrate --noinput
 
-if [ "$DEPLOYMENT_MODE" = "PRODUCTION" ];
-then
+if [ "${DEPLOYMENT_MODE}" = "PRODUCTION" ] || [  "${DEPLOYMENT_MODE}" = "STAGING"  ]; then
     python ./joplin/manage.py collectstatic --noinput
 else
-    echo "Static files will not be collected (migrated) to S3"
+    echo "Static files are only collected on STAGING or PRODUCTION, current environment: ${DEPLOYMENT_MODE}"
 fi;
