c839d3dd90d53c9d5960e6609ddc8327c8b889e5
Sergio <sergiogcx@gmail.com>
2018-10-01 13:54:57 -0500

    Troubleshooting backup data

--- .travis/heroku-helper.sh
+++ .travis/heroku-helper.sh
@@ -3,6 +3,13 @@
 # Tag used for testing
 TRAVIS_CI_TEST_TAG="travis-ci-internal-test"
 
+export PIPELINE_NAME_DEFAULT=devops-joplin-pipeline-testing
+export AWS_BUCKET_BACKUPS=joplin-austin-gov-archive
+export AWS_BUCKET_DEFAULT=joplin-austin-gov
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_BUCKET_REVIEWAPPS=joplin-austin-gov-prs3
+export PIPELINE_TEAM_DEFAULT=innovation-fellowship-program
+
 #
 # Pipeline Variables
 # These determine the exact pipeline in the heroku account where new PR (review apps) are created.
@@ -169,8 +176,12 @@ function joplin_parse_commit_message {
 
 function joplin_branch_to_prnumber {
 	BRANCH_NAME=$TRAVIS_BRANCH
-	PR_NUMBER=$(curl -X GET -s https://api.github.com/repos/cityofaustin/joplin/pulls | jq -r '.[] | "\(.head.ref):\(.number)"' | grep "${BRANCH_NAME}" | cut -d ":" -f 2)
-	echo "${PR_NUMBER}"
+	if [ "${BRANCH_NAME}" = "" ]; then
+	    echo "";
+    else
+        PR_NUMBER=$(curl -X GET -s https://api.github.com/repos/cityofaustin/joplin/pulls | jq -r '.[] | "\(.head.ref):\(.number)"' | grep "${BRANCH_NAME}" | cut -d ":" -f 2)
+        echo "${PR_NUMBER}"
+	fi;
 }
 
 
@@ -461,37 +472,6 @@ function joplin_backup_database {
 
     echo "This is where we are now: $?"
 
-    # Not a new PR, not a test, and not an error
-    if [ "$?" = "0" ]; then
-        exit 1;
-        # Retrieve App Name
-        joplin_build_header "Creating Database Backup in S3"
-
-        APPNAME=$(joplin_resolve_heroku_appname $1);
-
-        # Gather connection string from heroku api
-        CONNECTION_STRING=$(heroku config:get DATABASE_URL -a $APPNAME);
-        DB_NAME=$(echo -n $CONNECTION_STRING | cut -d "/" -f 4);
-        DB_TIMESTAMP=$(date '+%Y-%m-%d--%H-%M-%S');
-        DJANGO_MID=$(retrieve_latest_django_mid);
-
-        S3_BUCKET_FILE_URL="s3://${AWS_BUCKET_BACKUPS}/backups/database/${TRAVIS_BRANCH}/${APPNAME}.${DB_TIMESTAMP}.${TRAVIS_COMMIT}.${DJANGO_MID}.psql.gz"
-
-        echo "joplin_backup_database() ----- Performing Database Backup for Branch: $1, App: $APPNAME";
-        echo "joplin_backup_database() -- App Name: ${APPNAME}";
-        echo "joplin_backup_database() -- Date Timestamp: ${DB_TIMESTAMP}";
-        echo "joplin_backup_database() -- Latest Django Migration ID: ${DJANGO_MID}";
-        echo "joplin_backup_database() -- DB Name: ${DB_NAME}";
-        echo "joplin_backup_database() -- S3 File URL: ${S3_BUCKET_FILE_URL}";
-        echo "joplin_backup_database() -- Performing copy, please wait...";
-
-        pg_dump $CONNECTION_STRING | gzip | aws s3 cp - $S3_BUCKET_FILE_URL;
-        echo "joplin_backup_database()----- Finished creating and uploading database to s3";
-
-        echo "joplin_backup_database()----- Validating the backup has been created and is available on S3";
-        heroku_backup_upload_check $S3_BUCKET_FILE_URL
-        echo "joplin_backup_database()----- Validation finished, backup process finished.";
-    fi;
 }
 
 
