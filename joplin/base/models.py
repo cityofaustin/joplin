4c597531f9a1f097246eb130ad77f30185ce3d5d
easherma <ericandrewsherman@gmail.com>
2019-02-21 12:32:04 -0600

    add more format options

--- joplin/base/models.py
+++ joplin/base/models.py
@@ -20,7 +20,7 @@ from wagtail.search import index
 from . import blocks as custom_blocks
 from . import forms as custom_forms
 
-WYSIWYG_GENERAL = ['h1', 'h2', 'h3', 'link', 'ul', 'ol']
+WYSIWYG_GENERAL = ['h1', 'h2', 'h3', 'h4', 'bold', 'link', 'ul', 'ol']
 WYSIWYG_SERVICE_STEP = ['ul', 'ol', 'link']
 DEFAULT_MAX_LENGTH = 255
 
