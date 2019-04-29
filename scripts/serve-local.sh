388c4a1e1cc499227bed27ead1dedba56c977ccd
Niicck <niicck@users.noreply.github.com>
2019-04-29 16:42:29 -0500

    pass env variables to docker-compose

--- scripts/serve-local.sh
+++ scripts/serve-local.sh
@@ -7,9 +7,33 @@ set -o errexit
 HEROKU_KEY=$(heroku auth:token 2> /dev/null)
 
 if [ "$REBUILD" == "on" ]; then
-    docker build --no-cache -f Dockerfile.base -t joplin-base .
-    docker-compose -f docker-compose.local.yml up --build
+
+  export JOPLIN_APP_CONTAINER_NAME=joplin_app
+  export JOPLIN_DB_CONTAINER_NAME=joplin_db
+  export JOPLIN_ASSETS_CONTAINER_NAME=joplin_assets
+  export JOPLIN_DB_PUBLIC_PORT=5433
+  export JOPLIN_APP_PUBLIC_PORT=8000
+
+  docker build --no-cache -f Dockerfile.base -t joplin-base .
+  docker-compose -f docker-compose.local.yml up --build
+elif [ "$MIGRATIONS_TEST" == "on" ]; then
+
+  export JOPLIN_APP_CONTAINER_NAME=joplin_app_migration_test
+  export JOPLIN_DB_CONTAINER_NAME=joplin_db_migration_test
+  export JOPLIN_ASSETS_CONTAINER_NAME=joplin_assets_migration_test
+  export JOPLIN_DB_PUBLIC_PORT=5434
+  export JOPLIN_APP_PUBLIC_PORT=8000
+
+  docker build -f Dockerfile.base -t joplin-base . --build-arg MIGRATIONS_TEST=on
+  docker-compose -f docker-compose.local.yml up
 else
-    docker build -f Dockerfile.base -t joplin-base .
-    docker-compose -f docker-compose.local.yml up
+
+  export JOPLIN_APP_CONTAINER_NAME=joplin_app
+  export JOPLIN_DB_CONTAINER_NAME=joplin_db
+  export JOPLIN_ASSETS_CONTAINER_NAME=joplin_assets
+  export JOPLIN_DB_PUBLIC_PORT=5433
+  export JOPLIN_APP_PUBLIC_PORT=8000
+
+  docker build -f Dockerfile.base -t joplin-base .
+  docker-compose -f docker-compose.local.yml up
 fi
