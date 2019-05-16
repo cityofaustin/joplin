1260f54d7555647f58299ee13b19910630403069
Niicck <niicck@users.noreply.github.com>
2019-05-15 23:53:33 -0500

    build the migration test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-#
 # Docker Container Entry Point (Local)
 # This script sets the command and parameters that will be executed first when a container is started.
-#
-
-function load_latest_migration_datadump {
-  echo "Adding backup data"
-  python ./joplin/manage.py loaddata ./joplin/db/fixtures/migration_datadump_*.json
-}
 
 # We are on a local instance, wait until the deployment is available for connections.
 # Loop sleep every second until a connection is available.
@@ -25,7 +18,8 @@ python ./joplin/manage.py migrate --noinput
 
 if [ "$LOAD_DATA" = "on" ]; then
   # Seed test data into Joplin
-  load_latest_migration_datadump
+  echo "Adding backup data"
+  python ./joplin/manage.py loaddata ./joplin/db/data/migration_datadump_*.json
 else
   # Add initial admin user to Database if we aren't loading data
   echo "Adding test admin user for local development."
