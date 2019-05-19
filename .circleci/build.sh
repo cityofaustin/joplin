e5189f5a6ef215d337006aa54d2e8e78e62dd963
Niicck <niicck@users.noreply.github.com>
2019-05-19 17:10:13 -0500

    try pushing to coa's dockerhub repo

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -9,16 +9,24 @@ source $CURRENT_DIR/helpers.sh
 export DOCKER_BUILDKIT=1
 
 ENV=$(get_env)
+SHA=${CIRCLE_SHA1:0:7}
 
 if [ $ENV == "dev" ]; then
-  $DOCKER_TARGET="dev"
-  $DOCKER_TAG="joplin_app:dev"
+  $DOCKER_TARGET="joplin-dev"
+  $DOCKER_TAG_1="cityofaustin/joplin_app:dev-${CIRCLE_BRANCH}-latest"
+  $DOCKER_TAG_2="cityofaustin/joplin_app:dev-${CIRCLE_BRANCH}-${SHA}"
 elif [ $ENV == "staging" ]; then
-  $DOCKER_TARGET="staging"
-  $DOCKER_TAG="joplin_app:staging"
+  $DOCKER_TARGET="joplin-staging"
+  $DOCKER_TAG_1="cityofaustin/joplin_app:master-latest"
+  $DOCKER_TAG_2="cityofaustin/joplin_app:master-${SHA}"
 elif [ $ENV == "prod" ]; then
-  $DOCKER_TARGET="prod"
-  $DOCKER_TAG="joplin_app:prod"
+  $DOCKER_TARGET="joplin-prod"
+  $DOCKER_TAG_1="cityofaustin/joplin_app:production-latest"
+  $DOCKER_TAG_2="cityofaustin/joplin_app:production-${SHA}"
 fi
 
-docker build -f Dockerfile.app -t $DOCKER_TAG --target $DOCKER_TARGET $CURRENT_DIR/..
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker build -f Dockerfile.app -t $DOCKER_TAG_1 --target $DOCKER_TARGET $CURRENT_DIR/..
+docker tag $DOCKER_TAG_1 $DOCKER_TAG_2
+docker push $DOCKER_TAG_1
+docker push $DOCKER_TAG_2
