cf4dbea794d357ba392ed5404158b0e44b1fee01
Niicck <niicck@users.noreply.github.com>
2019-05-19 16:36:27 -0500

    use CIRCLE_BRANCH in build scripts

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
-DEPLOY_ENV=$1
 CURRENT_DIR=`dirname $BASH_SOURCE`
+
+source $CURRENT_DIR/helpers.sh
 
 # Allows us to use BUILDKIT features like adding a --target to docker build
 # Only works for "docker build" not "docker-compose ... --build"
 export DOCKER_BUILDKIT=1
 
-if [ $DEPLOY_ENV == "dev" ]; then
+ENV=$(get_env)
+
+if [ $ENV == "dev" ]; then
   $DOCKER_TARGET="dev"
   $DOCKER_TAG="joplin_app:dev"
-elif [ $DEPLOY_ENV == "staging" ]; then
+elif [ $ENV == "staging" ]; then
   $DOCKER_TARGET="staging"
   $DOCKER_TAG="joplin_app:staging"
-elif [ $DEPLOY_ENV == "prod" ]; then
+elif [ $ENV == "prod" ]; then
   $DOCKER_TARGET="prod"
   $DOCKER_TAG="joplin_app:prod"
 fi
 
-docker build -f Dockerfile.app -t $DOCKER_TAG --target $DOCKER_TARGET_APP $CURRENT_DIR/..
+docker build -f Dockerfile.app -t $DOCKER_TAG --target $DOCKER_TARGET $CURRENT_DIR/..
---
+++ .circleci/helpers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Determine ENV by your git branch
+function get_env {
+  if [ $CIRCLE_BRANCH == "master" ]; then
+    return "staging"
+  elif [ $CIRCLE_BRANCH == "production" ]; then
+    return "prod"
+  else
+    return "dev"
+}
