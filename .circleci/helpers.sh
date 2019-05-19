e908099481926e828079eddf770fec87f1dcb06d
Niicck <niicck@users.noreply.github.com>
2019-05-19 17:17:48 -0500

    fix helper script

--- .circleci/helpers.sh
+++ .circleci/helpers.sh
@@ -3,10 +3,10 @@
 # Determine ENV by your git branch
 function get_env {
   if [ $CIRCLE_BRANCH == "master" ]; then
-    return "staging"
+    echo "staging"
   elif [ $CIRCLE_BRANCH == "production" ]; then
-    return "prod"
+    echo "prod"
   else
-    return "dev"
+    echo "dev"
   fi
 }
