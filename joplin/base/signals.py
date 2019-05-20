377517a380a945925e94c62041ad7f0a18045704
Sergio Garcia <sergiogcx@gmail.com>
2019-05-20 12:02:04 -0500

    Added un-publish function hook (#237)
    * Added un-publish, default slack message
    
    * Cleaned-up output for signals

--- joplin/base/signals.py
+++ joplin/base/signals.py
@@ -32,7 +32,7 @@ def generate_responsive_images(sender, **kwargs):
         logger.debug(f'Generating image rendition for {width}px')
         image.get_rendition(f'width-{width}')
 
-def create_build_aws():
+def create_build_aws(instance):
     """
         Triggers a build in Amazon Elastic Container Service, it requires:
         1. DEPLOYMENT_MODE
@@ -46,6 +46,17 @@ def create_build_aws():
             - The name of the distribution for production.
             - IE: E455RV7LE5UVG
     """
+    logger.debug("create_build_aws() Starting task")
+
+    slack_message = ""
+
+    try:
+        slack_message = "'%s' was published by user: %s " % (instance.title, instance.owner.email)
+    except:
+        slack_message = ""
+
+    logger.debug("create_build_aws() Message: " + slack_message)
+
     if(settings.DEPLOYMENT_MODE in ["PRODUCTION", "STAGING"]):
 
         # First we initialize our AWS handler (client)
@@ -94,6 +105,10 @@ def create_build_aws():
                                 'name': 'CMS_MEDIA',
                                 'value': settings.JANIS_CMS_MEDIA  # 'https://joplin-austin-gov.s3.amazonaws.com/media'
                             },
+                            {
+                                'name': 'SLACK_MESSAGE',
+                                'value': slack_message
+                            }
                         ]
                     }
                 ]
@@ -147,10 +162,11 @@ def create_build(heroku, app, url, checksum=None, version=None, buildpack_urls=N
 def page_published_signal(sender, **kwargs):
     logger.debug(f'page_published {sender}')
     #create_build_if_configured()
-    create_build_aws()
+    create_build_aws(kwargs['instance'])
 
 
 @receiver(page_unpublished)
 def page_unpublished_signal(sender, **kwargs):
     logger.debug(f'page_unpublished {sender}')
-    create_build_if_configured()
+    #create_build_if_configured()
+    create_build_aws(kwargs['instance'])
