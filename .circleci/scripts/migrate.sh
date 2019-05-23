5d8fc890d77d94803042967a717bcda3bd4cc5cd
Niicck <niicck@users.noreply.github.com>
2019-05-23 15:51:59 -0500

    pass through exit code from heroku migration

--- .circleci/scripts/migrate.sh
+++ .circleci/scripts/migrate.sh
@@ -4,10 +4,10 @@ CURRENT_DIR=`dirname $BASH_SOURCE`
 source $CURRENT_DIR/helpers.sh
 log() { log_base "migrate" "$1" "$2"; }
 
-# APPNAME="joplin-pr-1836-migration-scrip" # for testing!
+APPNAME="joplin-pr-1836-migration-scrip" # for testing!
 
 print_header "Running Database Migration"
 
 # Manually run entrypoint script
 log 0 "Migrating data for App: ${APPNAME}";
-heroku run --app $APPNAME -- /app/docker-entrypoint.sh
+heroku run -x --app $APPNAME -- /app/docker-entrypoint.sh
