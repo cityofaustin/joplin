e89e8371c8375f411a1ac01767ceb4507d3819f9
Eric Sherman <ericandrewsherman@gmail.com>
2019-05-30 20:02:10 -0500

    trying some options for case sensitive stuff

--- joplin/users/models.py
+++ joplin/users/models.py
@@ -10,6 +10,13 @@ class UserManager(BaseUserManager):
 
     use_in_migrations = True
 
+    def get_by_natural_key(self, username):
+        return self.get(username__iexact = username)
+
+    # def get_by_natural_key(self, username):
+    #     case_insensitive_username_field = '{}__iexact'.format(self.model.USERNAME_FIELD)
+    #     return self.get(**{case_insensitive_username_field: username})
+
     def _create_user(self, email, password, **extra_fields):
         """Create and save a User with the given email and password."""
         if not email:
