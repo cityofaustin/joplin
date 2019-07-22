b2bf7dc8f10c7588b543e51dd2647484ac63b346
Niicck <niicck@users.noreply.github.com>
2019-07-22 12:12:01 -0500

    use LOAD_DATA flag in setup-undockered-data

--- scripts/setup-undockered-data.sh
+++ scripts/setup-undockered-data.sh
@@ -3,11 +3,16 @@
 set -o errexit
 CURRENT_DIR=`dirname $BASH_SOURCE`
 
+# This script runs data operations for an undockerized Joplin instance that
+# would otherwise have occured in docker-entrypoint.sh
+
 # Migrate
 pipenv run $CURRENT_DIR/../joplin/manage.py migrate --noinput
 
-# Load backup data
-pipenv run $CURRENT_DIR/../joplin/manage.py loaddata $CURRENT_DIR/../joplin/db/system-generated/seeding.datadump.json
+if [ "$LOAD_DATA" == "on" ]; then
+  # Load backup data
+  pipenv run $CURRENT_DIR/../joplin/manage.py loaddata $CURRENT_DIR/../joplin/db/system-generated/seeding.datadump.json
+fi
 
 # Load test admin
 pipenv run $CURRENT_DIR/../joplin/manage.py loaddata $CURRENT_DIR/../joplin/db/system-generated/local_admin_user.json
