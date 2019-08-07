f2468bd241db6881a57c5c07950bdc225bab4ec2
Brian Smith <briaguya@gmail.com>
2019-08-06 19:53:35 -0500

    don't set the icon highlighted if we're on a snippet

--- joplin/js/EditPage/menuActiveState.js
+++ joplin/js/EditPage/menuActiveState.js
@@ -1,6 +1,11 @@
-export default function () {
-    // Make the content index icon active in Edit Mode
-    // This decision came from Tori as she considers the edit pages as a sub-nav
-    // This would've required updates to the MenuItem class and its HTML template in Wagtail so I decided to write JS noodles.
-    $('.icon-home').closest('.menu-item').addClass('menu-active');
+export default function() {
+  // If we're on a snippet edit page don't turn on the pencil
+  if (window.location.href.includes('snippet')) return;
+
+  // Make the content index icon active in Edit Mode
+  // This decision came from Tori as she considers the edit pages as a sub-nav
+  // This would've required updates to the MenuItem class and its HTML template in Wagtail so I decided to write JS noodles.
+  $('.icon-home')
+    .closest('.menu-item')
+    .addClass('menu-active');
 }
