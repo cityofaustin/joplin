68ee743adb73c5433bde1821249d8eeb50c5bcb3
Eric Sherman <ericandrewsherman@gmail.com>
2019-09-03 14:24:48 -0500

    extra docs, try bumping version number

--- joplin/base/models/widgets.py
+++ joplin/base/models/widgets.py
@@ -1,13 +1,34 @@
 from countable_field import widgets
 
+"""
+copied from
+https://docs.google.com/spreadsheets/d/1ZkH93F4tJ0_Zjx4t8k6k6TgwSlOh8u8ejWmegE6WqH0/edit#gid=46035360
+TODO: come up with better names/structure to make these settings easy to refer to
+( the current names are based on the content model in google sheets)
+Should these be in constants or here where it's most relevant?
+
+These limits are NOT to be confused with character limits we might set in the database
+(Those limits may be greater)
+This allows for spillover/going negative on the character count and also helps keep tables consistent
+These limits are more about content than about data schema or performance
+"""
+
+
+GENERIC_TITLE_LIMIT = 58
+GENERIC_DESCRIPTION_LIMIT = 254
+
 AUTHOR_LIMITS = {
-    "title": 58,
+
+    "title": GENERIC_TITLE_LIMIT,
     "description": 254,
     "additional_content": 5000,
-    "document_title": 58,
-    "authoring_office": 58,
-    "summary": 600,
-    "mission": 400
+    "document_title": GENERIC_TITLE_LIMIT,
+    "authoring_office": GENERIC_TITLE_LIMIT,
+    "document_summary": 600,
+    "document_name": GENERIC_TITLE_LIMIT,
+    "mission": 400,
+    "about_director": 600,
+
 }
 
 countConfig = {'data-count': 'characters',
