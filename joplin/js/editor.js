16a1f1b7e8cd7286ed9ce3693599b5b35b472e5c
Brian Smith <briaguya@gmail.com>
2019-09-12 03:36:26 -0500

    run prettier on editor.js

--- joplin/js/editor.js
+++ joplin/js/editor.js
@@ -32,12 +32,12 @@ $(function() {
 
   // Get all labels and add styleguide links
   const labels = document.querySelectorAll('label');
-  const styleGuideUrl = djangoData.styleGuideUrl
+  const styleGuideUrl = djangoData.styleGuideUrl;
 
   // initialize state
   const state = {
-    currentLang: "en",
-    janisPreviewUrl: getPreviewUrl("en")
+    currentLang: 'en',
+    janisPreviewUrl: getPreviewUrl('en'),
   };
 
   for (const label of labels) {
@@ -147,15 +147,15 @@ $(function() {
   }
 
   function getPreviewUrl(currentLang) {
-    const previewUrlData = djangoData.previewUrlData
+    const previewUrlData = djangoData.previewUrlData;
     const janisUrlBase = previewUrlData.janis_url_base;
     const urlPageType = previewUrlData.url_page_type;
     const globalId = previewUrlData.global_id;
-    let janisPreviewUrl = djangoData.fallBackPreviewUrl
+    let janisPreviewUrl = djangoData.fallBackPreviewUrl;
     if (janisUrlBase && urlPageType && globalId) {
       janisPreviewUrl = `${janisUrlBase}/${currentLang}/preview/${urlPageType}/${globalId}`;
     }
-    return janisPreviewUrl
+    return janisPreviewUrl;
   }
 
   // Changes language and update janisPreviewUrl for our language
@@ -163,18 +163,18 @@ $(function() {
     state.currentLang = currentLang;
 
     var languageStrings = {
-      en: "[EN]",
-      es: "[ES]",
-      vi: "[VI]",
-      ar: "[AR]"
-    }
+      en: '[EN]',
+      es: '[ES]',
+      vi: '[VI]',
+      ar: '[AR]',
+    };
 
     var lowerLanguageStrings = {
-      en: "[en]",
-      es: "[es]",
-      vi: "[vi]",
-      ar: "[ar]"
-    }
+      en: '[en]',
+      es: '[es]',
+      vi: '[vi]',
+      ar: '[ar]',
+    };
 
     var languageRegex = /\[\w+\]/g;
 
@@ -182,39 +182,42 @@ $(function() {
     // This is hacky but it seems to be working
 
     // Top level fields
-    document
-      .querySelectorAll(".object")
-      .forEach(elem => {
-        if(elem.querySelectorAll(".title-wrapper").length) {
-          var headerText = elem.querySelectorAll(".title-wrapper")[0].innerText;
-          var langString = headerText.match(languageRegex);
-          if (langString != null && langString != languageStrings[currentLang] && langString != lowerLanguageStrings[currentLang]) {
-            elem.classList.add("hidden");
-          } else {
-            elem.classList.remove("hidden");
-          }
+    document.querySelectorAll('.object').forEach(elem => {
+      if (elem.querySelectorAll('.title-wrapper').length) {
+        var headerText = elem.querySelectorAll('.title-wrapper')[0].innerText;
+        var langString = headerText.match(languageRegex);
+        if (
+          langString != null &&
+          langString != languageStrings[currentLang] &&
+          langString != lowerLanguageStrings[currentLang]
+        ) {
+          elem.classList.add('hidden');
+        } else {
+          elem.classList.remove('hidden');
         }
-      });
+      }
+    });
 
     // Fields inside of InlinePanels
-    document
-    .querySelectorAll(".field")
-    .forEach(elem => {
-      if(elem.querySelectorAll("label").length) {
-        var labelText = elem.querySelectorAll("label")[0].innerText;
+    document.querySelectorAll('.field').forEach(elem => {
+      if (elem.querySelectorAll('label').length) {
+        var labelText = elem.querySelectorAll('label')[0].innerText;
         var langString = labelText.match(languageRegex);
 
-        if (langString != null && langString != lowerLanguageStrings[currentLang]) {
-          elem.parentElement.classList.add("hidden");
+        if (
+          langString != null &&
+          langString != lowerLanguageStrings[currentLang]
+        ) {
+          elem.parentElement.classList.add('hidden');
         } else {
-          elem.parentElement.classList.remove("hidden");
+          elem.parentElement.classList.remove('hidden');
         }
       }
     });
 
     // Select language radio button if it isn't set already
     // For instance, if the language change is triggered by a refresh from clicking Share or Preview
-    $(`#${currentLang}`).prop("checked", true);
+    $(`#${currentLang}`).prop('checked', true);
 
     // ----
     // Switch the language for janisPreviewUrl
@@ -226,41 +229,46 @@ $(function() {
     const sharePreviewUrl = $('#share-preview-url');
 
     // Update link for "Mobile Preview" button on sidebar
-    mobilePreviewSidebarButton.attr("href", janisPreviewUrl);
+    mobilePreviewSidebarButton.attr('href', janisPreviewUrl);
     sharePreviewUrl.text(janisPreviewUrl);
 
     // force reload of Mobile Preview iframe if its already open
-    if (_.includes(mobilePreviewSidebarButton[0].classList, "coa-sidebar-button--active")) {
-      $('#mobile-preview-iframe').attr("src", janisPreviewUrl)
+    if (
+      _.includes(
+        mobilePreviewSidebarButton[0].classList,
+        'coa-sidebar-button--active',
+      )
+    ) {
+      $('#mobile-preview-iframe').attr('src', janisPreviewUrl);
     }
   }
 
   var enButton = $('#en');
   enButton.click(function() {
-    changeLanguage("en");
+    changeLanguage('en');
   });
   var esButton = $('#es');
   esButton.click(function() {
-    changeLanguage("es");
+    changeLanguage('es');
   });
   var arButton = $('#ar');
   arButton.click(function() {
-    changeLanguage("ar");
+    changeLanguage('ar');
   });
   var viButton = $('#vi');
   viButton.click(function() {
-    changeLanguage("vi");
+    changeLanguage('vi');
   });
 
   // Initialize page in English, hide all other language fields
-  changeLanguage("en");
+  changeLanguage('en');
 
   // Persist language for preview even after page refreshes on save
   var previewbutton = $('#page-preview-button');
   if (localStorage.preview_lang) {
-    changeLanguage(localStorage.preview_lang)
+    changeLanguage(localStorage.preview_lang);
     window.open(state.janisPreviewUrl, '_blank');
-    localStorage.removeItem("preview_lang");
+    localStorage.removeItem('preview_lang');
   }
   previewbutton.click(function() {
     localStorage.preview_lang = state.currentLang;
@@ -277,7 +285,7 @@ $(function() {
 
     urlcopied.fadeOut(10000);
     localStorage.sharingpreview = false;
-    localStorage.removeItem("share_lang");
+    localStorage.removeItem('share_lang');
   }
   sharebutton.click(function() {
     localStorage.share_lang = state.currentLang;
@@ -285,10 +293,9 @@ $(function() {
 
   // Apply current language to new InlinePanels
   $('.add').click(function() {
-    changeLanguage(state.currentLang)
-  })
+    changeLanguage(state.currentLang);
+  });
 
   var messages = $('.messages');
   messages.fadeOut(10000);
-
 });
