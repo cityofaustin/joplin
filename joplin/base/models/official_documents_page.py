0851ca227ea09f727eaed90eed336769e2c18c93
Brian Smith <briaguya@gmail.com>
2019-09-27 01:20:48 -0500

    try nulling it?

--- joplin/base/models/official_documents_page.py
+++ joplin/base/models/official_documents_page.py
@@ -55,7 +55,7 @@ class OfficialDocumentPageOfficialDocument(Orderable):
     authoring_office = models.CharField(verbose_name="Authoring office of document", max_length=DEFAULT_MAX_LENGTH)
     summary = models.TextField(verbose_name="Document summary")
     name = models.CharField(verbose_name="Name of Document", max_length=DEFAULT_MAX_LENGTH)
-    document = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='+')
+    document = models.ForeignKey(Document, null=True, blank=False, on_delete=models.SET_NULL, related_name='+')
 
     panels = [
         FieldPanel('date'),
