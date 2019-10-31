5b55d478ea9b941af1f0aa7d21dedea505267dad
Nick Ivons <14187277+Niicck@users.noreply.github.com>
2019-10-31 12:49:09 -0500

    Revert "Revert "Master ---> Prod : Form Container""

--- joplin/base/context_processors.py
+++ joplin/base/context_processors.py
@@ -2,4 +2,11 @@ from django.conf import settings
 
 
 def settings_context(request):
-    return {'JANIS_URL': settings.JANIS_URL}
+    """
+    Django's preferred way of sending enviornment/build variables
+    into the app for templates and other contexual stuff
+    """
+    return {
+        'JANIS_URL': settings.JANIS_URL,
+        'ISPRODUCTION': settings.ISPRODUCTION
+    }
