b6f7e941586f28b3cb043f5ee8ad736e71e11be0
Niicck <niicck@users.noreply.github.com>
2019-10-30 21:56:20 -0500

    add documentation for LOAD_STAGING_DATA

--- scripts/download-media.sh
+++ scripts/download-media.sh
@@ -1,3 +1,9 @@
+# The default behavior of Joplin is to point to the remote staging CMS_MEDIA location,
+# which means you no longer need to download media files manually.
+# Previously, that was something we needed to do with this script.
+# It parsed a backup file from the db/smuggler directory and placed all referenced images into the local media folder.
+# It is no longer required as part of the development process.
+
 cat joplin/db/smuggler/pages.json | jq -r '.[].fields.file|select(.)' | while read line
 do
   if [ -f media/$line ]; then
