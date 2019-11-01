d56100440204f272d047c18af34900165688caee
Eric Sherman <ericandrewsherman@gmail.com>
2019-11-01 18:14:49 -0500

    working POC that blocks for any empty fields

--- joplin/base/forms.py
+++ joplin/base/forms.py
@@ -1,9 +1,27 @@
 from wagtail.admin.forms import WagtailAdminPageForm
+from wagtail.core.models import Page
+from django.core.exceptions import ValidationError
 
 
 class ServicePageForm(WagtailAdminPageForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def clean(self):
+        """
+        ways to limit scope:
+        changed fields,
+        then exclude fields that are required
+
+        self.changed_data = list of fields changed
+
+        self[field_name].data or as_text (might be useful for streamfields)
+        looks like this is working, atm tho it just wont let you publish any empty fields :-D
+        """
+        cleaned_data = super().clean()
+        field_keys = self.changed_data
+        for field_key in field_keys:
+            if not self[field_key].data:
+                self.add_error(field_key, "It's empty!")
+                ValidationError(('Invalid value, empty'), code='invalid')
+        return cleaned_data
 
 
 class ProcessPageForm(WagtailAdminPageForm):
@@ -39,6 +57,7 @@ class OfficialDocumentPageForm(WagtailAdminPageForm):
 class GuidePageForm(WagtailAdminPageForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
 
 class FormPageForm(WagtailAdminPageForm):
     def __init__(self, *args, **kwargs):
