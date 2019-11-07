1fd3ef984ddd555a4e933f073fb222045f54d30e
Eric Sherman <ericandrewsherman@gmail.com>
2019-11-06 19:08:32 -0600

    ugly but workable messages

--- joplin/base/forms.py
+++ joplin/base/forms.py
@@ -2,9 +2,25 @@ from wagtail.admin.forms import WagtailAdminPageForm
 from wagtail.core.models import Page, PageRevision
 from django.core.exceptions import ValidationError
 from wagtail.admin import messages
+from modelcluster.models import get_all_child_relations, get_all_child_m2m_relations
 
 
-class ServicePageForm(WagtailAdminPageForm):
+def get_children_values(some_object):
+    """
+    figured this out, may not actually need it tho
+    """
+    relations = []
+    for relation in get_all_child_relations(some_object):
+        # find out the property name used to access this relation
+        relation_name = relation.get_accessor_name()
+
+        # access that property on the current object to retrieve the related objects
+        children = getattr(some_object, relation_name).all()
+        relations.append(children)
+    return relations
+
+
+class JanisPageForm(WagtailAdminPageForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -21,50 +37,60 @@ class ServicePageForm(WagtailAdminPageForm):
         """
         def check_for_empties(form_entries):
             entries = []
-            # alt: self.fields[entry]
+            # alt structure to explore: self.fields[entry]
             for entry in form_entries:
                 # TODO really only works for strings atm, super lazy hack to ignore streamfields and prevent render error
                 if not self[entry].data and str(type(self.fields[entry])) == "<class 'django.forms.fields.CharField'>":
-                    self.add_error(entry, "It's empty!")
+                    self.add_error(entry, ValidationError((f'{entry} is empty!'), code='invalid'))
                     entries.append(entry)
-                    ValidationError(('Invalid value, empty'), code='invalid')
             return entries
+
+        def check_for_missing_relations():
+            relations = self.formsets
+            {
+                relation_name: self.add_error(None, f'No {relation_name} selected')
+                for (relation_name, value) in relations.items()
+                if not value.forms
+            }
 
         cleaned_data = super().clean()
 
         if 'action-publish' in self.data:
-            changed_fields = self.changed_data
-            check_changed = check_for_empties(changed_fields)
-            keys = list(self.fields.keys())
-            if not check_changed:
-                check_all = check_for_empties(keys)
+            # TODO: we'll probably want a good way to check a managed subset
+            all_keys = list(self.fields.keys())
+            check_all = check_for_empties(all_keys)
+            missing_relations = check_for_missing_relations()
         return cleaned_data
 
 
-class ProcessPageForm(WagtailAdminPageForm):
+class ServicePageForm(JanisPageForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
-class InformationPageForm(WagtailAdminPageForm):
+class ProcessPageForm(JanisPageForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
-class DepartmentPageForm(WagtailAdminPageForm):
+class InformationPageForm(JanisPageForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
-class TopicPageForm(WagtailAdminPageForm):
+class DepartmentPageForm(JanisPageForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def clean(self):
-        cleaned_data = super(TopicPageForm, self).clean()
-        if not self.instance.top_pages.exists():
-            raise ValidationError(('No top pages selected'), code='invalid')
-        return cleaned_data
+
+class TopicPageForm(JanisPageForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        def clean(self):
+            import pdb
+            pdb.set_trace()
+            cleaned_data = super(TopicPageForm, self).clean()
 
 
 class TopicCollectionPageForm(WagtailAdminPageForm):
