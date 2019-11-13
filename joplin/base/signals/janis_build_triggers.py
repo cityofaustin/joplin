58e3196f90289a5207de8a68cec150f2eb25f966
Eric Sherman <ericandrewsherman@gmail.com>
2019-11-13 14:34:49 -0600

    pass instance as param

--- joplin/base/signals/janis_build_triggers.py
+++ joplin/base/signals/janis_build_triggers.py
@@ -26,8 +26,10 @@ def trigger_build(sender, action='saved', instance=None):
     """
     trigger_object = instance
     logger.debug(f'{trigger_object} {action}, triggering build')
+    import pdb
+    pdb.set_trace()
     if settings.ISSTAGING or settings.ISPRODUCTION:
-        create_build_aws(sender, kwargs['instance'], request=get_http_request())
+        create_build_aws(sender, instance, request=get_http_request())
     elif settings.ISREVIEW:
         netlify_publish()
 
