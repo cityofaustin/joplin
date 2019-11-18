eaca39168d89424a46e10442099bce9497c30590
Eric Sherman <ericandrewsherman@gmail.com>
2019-11-18 17:12:45 -0600

    expose all location objects in modeladmin for fun

--- joplin/locations/wagtail_hooks.py
+++ joplin/locations/wagtail_hooks.py
@@ -1,16 +1,35 @@
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register)
 from .models import (
-    Location)
+    Location,
+    LocationPage,
+    LocationsIndexPage
+)
 
 
 class LocationAdmin(ModelAdmin):
     model = Location
-    menu_label = 'Location'  # ditch this to use verbose_primary_name_plural from model
+
     menu_icon = 'maps'  # change as required
     list_display = ('full_address',)
     list_filter = ('full_address',)
     search_fields = ('full_address',)
 
 
+class LocationPageAdmin(ModelAdmin):
+    model = LocationPage
+
+    menu_icon = 'maps'  # change as required
+    list_display = ('primary_name', 'physical_address')
+    list_filter = ('primary_name', 'physical_address')
+    search_fields = ('primary_name', 'physical_address')
+
+
+class LocationsIndexPageAdmin(ModelAdmin):
+    model = LocationsIndexPage
+    menu_icon = 'maps'  # change as required
+
+
 modeladmin_register(LocationAdmin)
+modeladmin_register(LocationPageAdmin)
+modeladmin_register(LocationsIndexPageAdmin)
