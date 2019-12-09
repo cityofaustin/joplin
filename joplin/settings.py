6d16e11cde4381ef64c533e6e4af8563fa220782
Eric Sherman <ericandrewsherman@gmail.com>
2019-12-09 12:46:58 -0600

    add memache (runs only locally atm?), to see if it affects mem usage

--- joplin/settings.py
+++ joplin/settings.py
@@ -103,6 +103,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'urls'
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
