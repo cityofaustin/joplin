5646fb21b941c6588eed04aa87c7d85f87a5d45b
Brian Smith <briaguya@gmail.com>
2020-01-07 16:45:22 -0600

    remove commented line

--- joplin/base/models/contact.py
+++ joplin/base/models/contact.py
@@ -13,7 +13,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
 
 from .location import Location
-# from locations.models import LocationPage
 from .day_and_duration import DayAndDuration
 
 from .constants import DEFAULT_MAX_LENGTH
