fade19dc508f5a78d416df29bf9aa78057111f45
Eric Sherman <ericandrewsherman@gmail.com>
2020-01-13 17:45:10 -0600

    more conservative number of conections
    hail mary
    
    conf tweaks
    
    more tweak
    
    try cutting to 10
    
    try another library

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,16 +2,23 @@ import os
 DEPLOYMENT_MODE = os.environ.get('DEPLOYMENT_MODE')
 
 worker_class = 'gevent'
+workers = 4
 # to stay under heroku limit of 20 connections
-worker_connections = 16
+worker_connections = 4
 
 pythonpath = "/app/joplin"
 reload = True
 #  see https://docs.gunicorn.org/en/stable/settings.html#max-requests
-max_requests = 100
+max_requests = 500
 max_requests_jitter = 50
 timeout = 190
 preload = True
 if DEPLOYMENT_MODE in ("LOCAL", "REVIEW"):
     timeout = 190
     loglevel = "DEBUG"
+
+from psycogreen.gevent import patch_psycopg
+
+def post_fork(server, worker):
+    patch_psycopg()
+    worker.log.info("Made Psycopg2 run using gevent (for async stuff)")
