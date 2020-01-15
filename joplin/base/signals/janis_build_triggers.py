806065fce4bc45d244a6781ba27a985358c5dea5
Chia Berry <chia.berry@austintexas.gov>
2020-01-15 10:27:58 -0600

    single page, pages from snippets

--- joplin/base/signals/janis_build_triggers.py
+++ joplin/base/signals/janis_build_triggers.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from wagtail.core.signals import page_published, page_unpublished
+from graphene import Node
 
 import heroku3
 from heroku3.models.build import Build
@@ -19,15 +20,14 @@ logger = logging.getLogger(__name__)
 JANIS_SLUG_URL = settings.JANIS_SLUG_URL
 
 
-def trigger_build(sender, action='saved', instance=None):
-    print(settings.ISLOCAL)
-    print(sender)
-    print(sender.id)
-    # page_global_id = graphene.Node.to_global_id(content_type_data["node"], page.id)
-    # >>> glob_id = Node.to_global_id('InformationPage', 28)
-    # >>> glob_id
-#SW5mb3JtYXRpb25QYWdlOjI4'
-    # then see about passing it into the create build aws
+def trigger_build(sender, pages_ids, action='saved', instance=None):
+    print(pages_ids)
+    # >> > graphene.Node.to_global_id('InformationPage', 28)
+    # 'SW5mb3JtYXRpb25QYWdlOjI4'
+    # >> > graphene.Node.to_global_id('Information page', 28)
+    # 'SW5mb3JtYXRpb24gcGFnZToyOA=='
+    # >> > graphene.Node.to_global_id('information', 28)
+    # 'aW5mb3JtYXRpb246Mjg='
     """
     triggers different build process depending on environment
     source = name of snippet or object triggering build
@@ -48,19 +48,29 @@ def trigger_build(sender, action='saved', instance=None):
 @receiver(post_save, sender=Location)
 @receiver(post_save, sender=Map)
 def handle_post_save_signal(sender, **kwargs):
-    trigger_build(sender, instance=kwargs['instance'])
+    usage = kwargs['instance'].get_usage()
+    pages_ids = []
+    for p in usage:
+        page_id = Node.to_global_id(p.content_type.name, p.id)
+        pages_ids.append(page_id)
+
+    trigger_build(sender, pages_ids, instance=kwargs['instance'])
 
 
 @receiver(page_published)
 def page_published_signal(sender, **kwargs):
-    trigger_build(sender, action='published', instance=kwargs['instance'])
+    # this does not take into account all the pages its connected to
+    page_global_id = Node.to_global_id(kwargs['instance'].get_verbose_name(), kwargs['instance'].id)
+    trigger_build(sender, page_global_id, action='published', instance=kwargs['instance'])
 
 
 @receiver(page_unpublished)
 def page_unpublished_signal(sender, **kwargs):
-    trigger_build(sender, action='unpublished', instance=kwargs['instance'])
+    page_global_id = Node.to_global_id(kwargs['instance'].get_verbose_name(), kwargs['instance'].id)
+    trigger_build(sender, page_global_id, action='unpublished', instance=kwargs['instance'])
 
-# TODO: should we add hooks for the above snippets/models on post delete as well?
+# TODO: should we add hooks for the above snippets/models on post delete as well? <--- ?
 @receiver(post_delete, sender=Document)
 def document_post_delete_signal(sender, **kwargs):
-    trigger_build(sender, action='deleted', instance=kwargs['instance'])
+    page_global_id = Node.to_global_id(kwargs['instance'].get_verbose_name(), kwargs['instance'].id)
+    trigger_build(sender, page_global_id, action='deleted', instance=kwargs['instance'])
