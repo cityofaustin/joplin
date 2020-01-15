c793fbb00dea7c5550ac1d5580258cffb46548a6
Eric Sherman <ericandrewsherman@gmail.com>
2020-01-14 18:03:09 -0600

    conf tweaks

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,13 +2,14 @@ import os
 DEPLOYMENT_MODE = os.environ.get('DEPLOYMENT_MODE')
 
 worker_class = 'gevent'
+workers = 4
 # to stay under heroku limit of 20 connections
-worker_connections = 3
+worker_connections = 1000
 
 pythonpath = "/app/joplin"
 reload = True
 #  see https://docs.gunicorn.org/en/stable/settings.html#max-requests
-max_requests = 100
+max_requests = 500
 max_requests_jitter = 50
 timeout = 190
 preload = True
