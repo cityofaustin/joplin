ce7d64ab125eed3039a3c6ce9ea703554adb8a57
Brian Smith <briaguya@gmail.com>
2020-01-22 19:32:25 -0600

    make it better?

--- joplin/base/translation.py
+++ joplin/base/translation.py
@@ -5,6 +5,7 @@ from wagtail.images.models import Image
 
 from .models import ThreeOneOne, TopicPage, TopicCollectionPage, Theme, ServicePage, ProcessPage, ProcessPageStep, DepartmentPage, DepartmentPageDirector, InformationPage, OfficialDocumentPage, OfficialDocumentPageOfficialDocument, TranslatedImage, Department, Map, HomePage, GuidePage, FormContainer
 from locations.models import LocationPage
+from events.models import EventPage
 
 
 @register(Image)
@@ -159,3 +160,10 @@ class LocationPageOptions(TranslationOptions):
     fields = (
         'hours_exceptions',
     )
+
+@register(EventPage)
+class EventPageTranslationOptions(TranslationOptions):
+    fields = (
+        'description',
+    )
+
