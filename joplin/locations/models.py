416d5a0b003d9bb8710b1bb02244c5113d52addd
Eric Sherman <ericandrewsherman@gmail.com>
2020-01-27 11:42:27 -0600

    merge from master tweak

--- joplin/locations/models.py
+++ joplin/locations/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.images.models import Image
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, HelpPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from phonenumber_field.modelfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
-from base.models import HomePage
+from wagtail.core.models import Orderable
 from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import (
     InlinePanel,
@@ -14,13 +13,9 @@ from wagtail.admin.edit_handlers import (
 )
 from base.models.translated_image import TranslatedImage
 
-# The abstract model for related links, complete with panels
-from wagtail.search import index
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, HelpPanel
-from wagtail.images.edit_handlers import ImageChooserPanel
+
 from base.models import JanisBasePage
-from base.models.widgets import countMe, countMeTextArea, AUTHOR_LIMITS
-from modelcluster.models import ClusterableModel
+from base.models.widgets import countMe
 from base.models.constants import DEFAULT_MAX_LENGTH
 
 
