3291193ea62c164274d0ba51f5d3e1747686b00f
Niicck <niicck@users.noreply.github.com>
2020-02-04 13:40:31 -0600

    try to get bash_source

--- .circleci/scripts/run_tests.sh
+++ .circleci/scripts/run_tests.sh
@@ -2,5 +2,8 @@
 set -o errexit
 
 # pipenv run sh .circleci/scripts/run_tests.sh
+echo "What is BASH_SOURCE? [$BASH_SOURCE]"
+echo "What is BASH_SOURCE[0]? [${BASH_SOURCE[0]}]"
+echo "What is BASH_SOURCE[*]? [${BASH_SOURCE[*]}]"
 CURRENT_DIR=`dirname $BASH_SOURCE`
 python $CURRENT_DIR/../../joplin/manage.py test publish_preflight.tests.information_page_test
