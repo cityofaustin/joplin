d0261a06c860a5280dd7d8246e7d066f06e2de91
Niicck <niicck@users.noreply.github.com>
2020-02-04 13:21:53 -0600

    run_tests.sh should be bash

--- .circleci/scripts/run_tests.sh
+++ .circleci/scripts/run_tests.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -o errexit
+
 # pipenv run sh .circleci/scripts/run_tests.sh
 CURRENT_DIR=`dirname $BASH_SOURCE`
 python $CURRENT_DIR/../../joplin/manage.py test publish_preflight.tests.information_page_test
