bca167dd9b0bdc6efbc08a3d7d8886ef0ada1047
Niicck <niicck@users.noreply.github.com>
2020-02-05 10:30:32 -0600

    fix bug in load_joplin_data paths

--- joplin/base/management/commands/load_joplin_data.py
+++ joplin/base/management/commands/load_joplin_data.py
@@ -5,6 +5,7 @@ from django.core.management import call_command
 from django.core.exceptions import ObjectDoesNotExist
 from base.models import DeploymentLog
 from django.db import connection
+from django.conf import settings
 
 class Command(BaseCommand):
     help = "Load initial seeding data into your app"
@@ -13,7 +14,8 @@ class Command(BaseCommand):
         stdout = StringIO()
         stderr = StringIO()
 
-        def run_load_data_command(filepath):
+        def run_load_data_command(file):
+            filepath = os.path.join(settings.BASE_DIR, 'joplin', file)
             call_command('loaddata', filepath, stdout=stdout, stderr=stderr)
             if stdout.getvalue():
                 print(stdout.getvalue())
@@ -37,23 +39,23 @@ class Command(BaseCommand):
                 DATABASE_URL = os.getenv("DATABASE_URL")
                 if (LOAD_DATA == "prod"):
                     print("Adding prod datadump")
-                    run_load_data_command('./joplin/db/system-generated/prod.datadump.json')
+                    run_load_data_command('db/system-generated/prod.datadump.json')
                     DeploymentLog(operation="load_data", value="prod", completed=True).save()
                 elif (LOAD_DATA == "staging"):
                     print("Adding staging datadump")
-                    run_load_data_command('./joplin/db/system-generated/staging.datadump.json')
+                    run_load_data_command('db/system-generated/staging.datadump.json')
                     DeploymentLog(operation="load_data", value="staging", completed=True).save()
                 elif (LOAD_DATA == "dummy"):
                     print("Adding dummy datadump")
-                    run_load_data_command('./joplin/db/system-generated/dummy.datadump.json')
+                    run_load_data_command('db/system-generated/dummy.datadump.json')
                     DeploymentLog(operation="load_data", value="dummy", completed=True).save()
                 elif (LOAD_DATA == "new_datadump"):
                     print("Adding new migration test datadump")
-                    run_load_data_command('./joplin/db/system-generated/tmp.datadump.json')
+                    run_load_data_command('db/system-generated/tmp.datadump.json')
 
                     # Runs code from /db/scripts/sanitize_revision_data.sql
                     print("Sanitizing Revisions data")
-                    sanitize_revision_path = os.path.join(os.path.dirname(__file__), f'../../../db/scripts/sanitize_revision_data.sql')
+                    sanitize_revision_path = os.path.join(settings.BASE_DIR, 'joplin/db/scripts/sanitize_revision_data.sql')
                     sanitize_revision_file = open(sanitize_revision_path, 'r')
                     sanitize_revision_sql = sanitize_revision_file.read()
                     sanitize_revision_file.close()
@@ -77,7 +79,7 @@ class Command(BaseCommand):
                 (os.getenv("DEPLOYMENT_MODE") == "LOCAL")
             ):
                 print("Adding test admin user for development.")
-                run_load_data_command('./joplin/db/fixtures/local_admin_user.json')
+                run_load_data_command('db/fixtures/local_admin_user.json')
                 DeploymentLog(operation="load_test_admin", completed=True).save()
 
             # Load janis_branch_settings if they haven't been loaded already
@@ -93,7 +95,7 @@ class Command(BaseCommand):
                 not os.getenv("DEPLOYMENT_MODE") in ("STAGING", "PRODUCTION")
             ):
                 print("Adding Janis Branch settings")
-                run_load_data_command('./joplin/db/fixtures/janis_branch_settings.json')
+                run_load_data_command('db/fixtures/janis_branch_settings.json')
                 DeploymentLog(operation="load_janis_branch_settings", completed=True).save()
 
             # Set group_permissions if they haven't been loaded already
@@ -109,7 +111,7 @@ class Command(BaseCommand):
                 not os.getenv("DEPLOYMENT_MODE") in ("STAGING", "PRODUCTION")
             ):
                 print("Setting editor and moderator group permissions")
-                run_load_data_command('./joplin/db/fixtures/group_permissions_settings.json')
+                run_load_data_command('db/fixtures/group_permissions_settings.json')
                 DeploymentLog(operation="set_group_permissions", completed=True).save()
         finally:
             stdout.close()
