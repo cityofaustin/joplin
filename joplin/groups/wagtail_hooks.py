f626cb2616f6d5ba7e2551a22f6174bbc0afb718
Brian Smith <briaguya@gmail.com>
2020-02-25 22:20:11 -0600

    clean up department editing

--- joplin/groups/wagtail_hooks.py
+++ joplin/groups/wagtail_hooks.py
@@ -5,13 +5,10 @@ from .models import Department
 
 class DepartmentAdmin(ModelAdmin):
     model = Department
-    menu_icon = 'pilcrow'  # change as required
+    menu_icon = 'group'  # change as required
     menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
-    list_display = ('name',)
-    list_filter = ('name',)
-    search_fields = ('name',)
 
 
 modeladmin_register(DepartmentAdmin)
