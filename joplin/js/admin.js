26d79e67fa0d4e83bc251a8cc9993d7128923465
Bob Main <bobmain49@gmail.com>
2020-03-03 09:30:59 -0600

    lasypush

--- joplin/js/admin.js
+++ joplin/js/admin.js
@@ -1,4 +1,4 @@
-//import '../css/admin.scss';
+// import '../css/admin.scss';
 
 if (window.ISPRODUCTION !== "False") { // 🚨revert to simly `import '../joplin_UI/admin.scss'` after Joplin_UI is updated to prod
   require('../css/admin.scss');
