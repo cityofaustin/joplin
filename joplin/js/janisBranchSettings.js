9080930dec677406bc3f5578cc410456ab3b50ba
Brian Smith <briaguya@gmail.com>
2020-03-06 06:01:46 -0600

    make buttons work

--- joplin/js/janisBranchSettings.js
+++ joplin/js/janisBranchSettings.js
@@ -1,22 +1,22 @@
 // If "URL" is selected, then only show input field for preview_janis_url
 // Else, "Branch Name" is selected and we should show field for preview_janis_branch
-const togglePreviewSettings = (previewInputValue) => {
-  if (previewInputValue === "url") {
-    $(".preview_janis_url-container").show()
-    $(".preview_janis_branch-container").hide()
+const togglePreviewSettings = previewInputValue => {
+  if (previewInputValue === 'url') {
+    $('.preview_janis_url-container').show();
+    $('.preview_janis_branch-container').hide();
   } else {
-    $(".preview_janis_url-container").hide()
-    $(".preview_janis_branch-container").show()
+    $('.preview_janis_url-container').hide();
+    $('.preview_janis_branch-container').show();
   }
-}
+};
 
 $(function() {
   // Only run code when editing janisbranchsettings
-  if (location.pathname === "/admin/settings/base/janisbranchsettings/2/") {
-    const previewInputNode = $("select#id_preview_input")
-    togglePreviewSettings(previewInputNode.val())
+  if (location.pathname === '/admin/settings/base/janisbranchsettings/1/') {
+    const previewInputNode = $('select#id_preview_input');
+    togglePreviewSettings(previewInputNode.val());
     previewInputNode.change(function() {
-      togglePreviewSettings($(this).val())
-    })
+      togglePreviewSettings($(this).val());
+    });
   }
-})
+});
