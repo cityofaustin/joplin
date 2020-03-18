089bb9ffa8c69bb35e46d9682c16b47e315e6b1b
Brian Smith <briaguya@gmail.com>
2020-03-18 18:48:02 -0500

    clean up queries from comments

--- joplin/importer/queries.py
+++ joplin/importer/queries.py
@@ -2,7 +2,7 @@ from gql import gql
 
 queries = {
     'topiccollection': gql('''
-    query getTopicPageRevision($id: ID) {
+    query getTopicCollectionPageRevision($id: ID) {
       allPageRevisions(id: $id) {
         edges {
           node {
@@ -54,40 +54,6 @@ queries = {
                       }
                     }
                   }
-                }
-              }
-            }
-          }
-        }
-    '''),
-    'information': gql('''
-        query getInformationPageRevision($id:ID) {
-          allPageRevisions(id:$id) {
-            edges {
-              node {
-                asInformationPage {
-                  id
-                  title
-                  topics {
-                    edges {
-                      node {
-                        id
-                      }
-                    }
-                  }
-                  departments {
-                    id
-                  }
-                  description
-                  additionalContent
-                  contacts {
-                    edges {
-                      node {
-                        id
-                      }
-                    }
-                  }
-                  coaGlobal
                 }
               }
             }
