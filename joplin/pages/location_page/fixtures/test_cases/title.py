9ff8b5342b849c8e0af3a26e5c6e3eb259780b46
Bob Main <bobmain49@gmail.com>
2020-03-31 13:23:43 -0500

    adding basic fixtures

--- joplin/pages/location_page/fixtures/test_cases/title.py
+++ joplin/pages/location_page/fixtures/test_cases/title.py
@@ -3,8 +3,20 @@ from pages.location_page.fixtures.helpers.create_fixture import create_fixture
 import pages.location_page.fixtures.helpers.components as components
 
 
-# A Service page with only a title
+# A Location page with only a title
 def title():
+    page_data = {
+        "imported_revision_id": None,
+        "live": False,
+        "parent": components.home(),
+        "coa_global": False,
+        "title": "Location page with title",
+        "slug": "location-page-with-title",
+    }
+
+    return create_fixture(page_data, os.path.basename(__file__))
+
+def title_and_address():
     page_data = {
         "imported_revision_id": None,
         "live": False,
