33e95ca83d85d2715716d0ef00e0a1ca994ff259
Niicck <niicck@users.noreply.github.com>
2020-03-30 22:23:08 -0500

    add error messages for user requirements

--- joplin/users/js/index.js
+++ joplin/users/js/index.js
@@ -1,11 +1,10 @@
 $(function() {
-  const userGroups = JSON.parse(document.getElementById('user-groups').textContent);
   /**
     Our User form only knows about values for "Groups".
     So we need to parse the Groups that are set for that user,
     and set the values for "Roles" and "Departments" on the Form's UI.
 
-    This only needs to be done for "wagtailusers/users/edit" template.
+    This code will only impact the "wagtailusers/users/edit" template.
     The "wagtailusers/users/create" template will not have existing Group data,
     because its creating a new user.
 
@@ -14,11 +13,31 @@ $(function() {
     A Group larger than 2 will be a Department.
     That is selected by entering the value in the dropdown select field.
   **/
-  userGroups.forEach(group => {
-    if ([1,2].includes(group)) {
-      $(`input[name='roles'][value=${group}]`).prop("checked", true)
-    } else if (group > 2) {
-      $("select[name='department']").val(group)
+  const userGroupsRaw = $('#user-groups').text()
+  if (userGroupsRaw) {
+    const userGroups = JSON.parse(userGroupsRaw);
+    userGroups.forEach(group => {
+      if ([1,2].includes(group)) {
+        $(`input[name='roles'][value=${group}]`).prop("checked", true)
+      } else if (group > 2) {
+        $("select[name='department']").val(group)
+      }
+    })
+  }
+
+  // Insert error messages into form
+  const userErrorsRaw = $('#user-errors').text()
+  if (userErrorsRaw) {
+    const userErrors = JSON.parse(userErrorsRaw);
+    if (userErrors.department) {
+      const departmentParent = $($('label:contains("Department:")')[0]).parent()
+      const departmentMessage = $(`<div class='coa-user-form-error'>${userErrors.department}</div>`)
+      departmentMessage.insertBefore(departmentParent)
     }
-  })
+    if (userErrors.roles) {
+      const rolesParent = $($('label:contains("Roles:")')[0]).parent()
+      const rolesMessage = $(`<div class='coa-user-form-error'>${userErrors.roles}</div>`)
+      rolesMessage.insertBefore(rolesParent)
+    }
+  }
 })
