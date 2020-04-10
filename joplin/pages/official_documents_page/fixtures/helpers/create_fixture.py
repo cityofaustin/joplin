37e33c60add9a04b5d83e2fcc8c7c03fa3e0b0f6
Brian Smith <briaguya@gmail.com>
2020-04-10 08:29:20 -0500

    this is probably broken but let's see what's up with it

--- joplin/pages/official_documents_page/fixtures/helpers/create_fixture.py
+++ joplin/pages/official_documents_page/fixtures/helpers/create_fixture.py
@@ -1,6 +1,8 @@
 from pages.official_documents_page.models import OfficialDocumentPage
-from pages.official_documents_page.factories import OfficialDocumentPageFactory
-
+from wagtail.documents.models import Document
+from pages.official_documents_page.factories import OfficialDocumentPageFactory, DocumentFactory
+import hashlib
+from django.core.files.base import ContentFile
 
 # Skips creating fixture if Page with slug already exists
 def create_fixture(page_data, fixture_name):
@@ -15,3 +17,20 @@ def create_fixture(page_data, fixture_name):
     page = OfficialDocumentPageFactory.create(**page_data)
     print(f"Built topic page fixture: {fixture_name}")
     return page
+
+
+def create_fixture_document(file_content, file_name):
+    # add a document
+    file_hash = hashlib.sha1(file_content).hexdigest()
+    # Check if a document with the same hash has already been imported
+    try:
+        document = Document.objects.get(file_hash=file_hash)
+    except Document.DoesNotExist:
+        document = None
+    if document:
+        print(f"Skipping document fixture: {file_name}")
+        return document
+
+    document = DocumentFactory.create(file=ContentFile(file_content, name=file_name), title=file_name)
+    print(f"Built document fixture: {file_name}")
+    return document
--- joplin/pages/official_documents_page/fixtures/test_cases/kitchen_sink.py
+++ joplin/pages/official_documents_page/fixtures/test_cases/kitchen_sink.py
@@ -1,5 +1,6 @@
+import hashlib
 import os
-from pages.official_documents_page.fixtures.helpers.create_fixture import create_fixture
+from pages.official_documents_page.fixtures.helpers.create_fixture import create_fixture, create_fixture_document
 import pages.official_documents_page.fixtures.helpers.components as components
 from pages.topic_page.fixtures.test_cases import kitchen_sink as kitchen_sink_topic
 
@@ -7,6 +8,15 @@ from pages.topic_page.fixtures.test_cases import kitchen_sink as kitchen_sink_to
 # A "kitchen sink" official documents page
 def kitchen_sink():
     topic = kitchen_sink_topic.kitchen_sink()
+
+    # add a document
+    file_name = 'filename.txt'
+    file_content = "I am file content"
+    document = create_fixture_document(file_content, file_name)
+    official_documents_page_document_node = {
+        'date': "1/1/2021",
+        'document': document
+    }
 
     page_data = {
         "imported_revision_id": None,
@@ -20,7 +30,8 @@ def kitchen_sink():
             "topics": [topic]
         },
         "description": "Kitchen sink official documents page description [en]",
-        "description_es": "Kitchen sink official documents page description [es]"
+        "description_es": "Kitchen sink official documents page description [es]",
+        'add_official_documents_page_documents': {'official_documents_page_documents': [official_documents_page_document_node]}
     }
 
     return create_fixture(page_data, os.path.basename(__file__))
