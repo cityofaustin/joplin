1576496cc9f3a94bb504d5a741127f7024ef33dd
Brian Smith <briaguya@gmail.com>
2020-04-14 03:48:05 -0500

    they're working! let's fix the tests

--- joplin/pages/service_page/factories.py
+++ joplin/pages/service_page/factories.py
@@ -11,33 +11,26 @@ class ServicePageFactory(JanisBasePageWithTopicsFactory):
         for step_keyword in step_keywords:
             steps = kwargs.pop(step_keyword, [])
 
-            steps_to_format = []
+            formatted_steps = []
             for step in steps:
-                step_to_format = {'type': step['type']}
+                formatted_step = {'type': u'{0}'.format(step['type'])}
                 if step['type'] == 'step_with_options_accordian':
-                    step_to_format['value'] = json.dumps({
-                        u'options': u'{0}'.format(json.dumps([
+                    formatted_step['value'] = {
+                        'options': [
                             {
                                 u'option_description': u'{0}'.format(option['option_description']),
                                 u'option_name': u'{0}'.format(option['option_name'])
                             }
                             for option in step['value']['options']
-                        ])),
+                        ],
                         u'options_description': u'{0}'.format(step['value']['options_description']),
-                    })
-                    # step_to_format['value'] = step['value']
+                    }
                 else:
-                    step_to_format['value'] = step['value']
-                steps_to_format.append(step_to_format)
+                    formatted_step['value'] = u'{0}'.format(step['value'])
+                formatted_steps.append(formatted_step)
 
-            formatted_steps = json.dumps([
-                {
-                    u'type': u'{0}'.format(step['type']),
-                    u'value': u'{0}'.format(step['value'])
-                }
-                for step in steps_to_format
-            ])
-            kwargs[step_keyword] = formatted_steps
+            json_steps = json.dumps(formatted_steps)
+            kwargs[step_keyword] = json_steps
         return super(ServicePageFactory, cls).create(*args, **kwargs)
 
 
