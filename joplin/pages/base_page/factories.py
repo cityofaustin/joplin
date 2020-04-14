fe7cb65cd0f2a5dd7dd2f29d7facec3d05c866fa
Brian Smith <briaguya@gmail.com>
2020-04-13 23:18:23 -0500

    clean up and see what's good

--- joplin/pages/base_page/factories.py
+++ joplin/pages/base_page/factories.py
@@ -8,45 +8,15 @@ class JanisBasePageFactory(PageFactory):
     class Meta:
         model = JanisBasePage
 
-
     @factory.post_generation
     def add_departments(self, create, extracted, **kwargs):
         # Import here to avoid circular dependencies for the DepartmentPage
         from groups.factories import GroupPagePermissionFactory
 
-        # TODO: add option to pass in already created departments
         if extracted:
             # A list of departments were passed in, use them
             for department in extracted['departments']:
-                # Check if a department with the same ????? has already been imported
-                try:
-                    # todo: create department groups
-                    # slug we want to make is kitchen-sink-department
-                    depts = Department.objects.all()
-                    for dept in depts:
-                        blarg = 4
-                    department_group = Department.objects.get(department_page__slug=department.slug)
-                    # document = Document.objects.get(file_hash=file_hash)
-                except Department.DoesNotExist:
-                    pass
-                    # document = None
-                # if document:
-                    # return document
-
-                # It has not been imported, let's do it!
-                document = DocumentFactory.create(file=ContentFile(response.content, name=file_name), title=file_name)
-                return document
-
-
-
-
-                GroupPagePermissionFactory.create(page=self, )
-            return
-
-
-            # for related_department in extracted:
-                # todo: add department stuff here
-                # GroupPagePermissionFactory.create(page=self, topic_collection=topic_collection)
+                GroupPagePermissionFactory.create(page=self, group=department)
             return
 
         # pass "add_department__dummy"=True into Factory() to make dummy departments
