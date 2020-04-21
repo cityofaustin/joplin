0ebc28412c0cb9cea77c4e323b5631f12cb0e354
Niicck <niicck@users.noreply.github.com>
2020-04-20 19:29:57 -0500

    create root home_page in a fixture, instead of migration

--- joplin/conftest.py
+++ joplin/conftest.py
@@ -3,11 +3,12 @@ import pytest
 import inspect
 import os
 from factory.base import FactoryMetaClass
-import pages.home_page.fixtures as home_page_fixtures
 import json
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
+import pages.home_page.fixtures as home_page_fixtures
+import base.fixtures.administrative.mandatory_fixtures as mandatory_fixtures
 
 # from django.core.management import call_command
 # @pytest.fixture(scope='session')
@@ -31,6 +32,7 @@ def test_suite_cleanup_thing(request):
     # Clear test_api_jwt_token at start of running entire test suite.
     # This will ensure that our test_api_jwt_token is only cached for 1 pytest invocation and won't expire.
     request.config.cache.set('test_api_jwt_token', None)
+    mandatory_fixtures.load_all()
     yield
     # teardown
 
--- joplin/pages/home_page/fixtures/__init__.py
+++ joplin/pages/home_page/fixtures/__init__.py
@@ -1,5 +1,5 @@
-from .test_cases.pytest import pytest
+from pages.home_page.fixtures.test_cases.pytest import pytest
+from pages.home_page.fixtures.administrative.root_home_page import root_home_page
 
 # We do not have a load_all() function for home_pages.
-# Though we could, we generally do not want to load all home_page fixtures.
-# The normal base HomePage is already preloaded by a migration.
+# Though we could, we probably do not want to load all home_page fixtures.
