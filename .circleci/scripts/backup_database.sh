7722939cdc2b0a8ca1fec84333542ed7f031309b
Brian Smith <briaguya@gmail.com>
2020-04-22 16:56:30 -0500

    we don't have a db to backup

--- .circleci/scripts/backup_database.sh
+++ .circleci/scripts/backup_database.sh
@@ -8,29 +8,29 @@ log() { log_base "backup_database" "$1" "$2"; }
 APP_DB_EXISTS=$(app_database_attached $APPNAME)
 
 if [ $APP_DB_EXISTS == "true" ]; then
-
-  print_header "Creating Database Backup in S3"
-
-  # Create a new backup
-  heroku pg:backups:capture -a $APPNAME
-
-  # Download the new backup
-  LOCAL_DUMP_FILENAME=$APPNAME.dump
-  heroku pg:backups:download -a $APPNAME -o $LOCAL_DUMP_FILENAME
-
-  # upload to s3
-  DB_CONNECTION_STRING=$(heroku config:get DATABASE_URL -a $APPNAME);
-  LATEST_MIGRATION=$(psql $DB_CONNECTION_STRING -qtA -c 'select name from django_migrations order by id desc limit 1;')
-  BACKUP_TIMESTAMP=$(date '+%Y-%m-%d--%H-%M-%S')
-  S3_DUMP_FILENAME="${APPNAME}.${BACKUP_TIMESTAMP}.${SHA}.${LATEST_MIGRATION}.dump"
-  S3_BUCKET_FILE_URL="s3://joplin-austin-gov-archive/deployment-backups/database/${APPNAME}/${S3_DUMP_FILENAME}"
-
-  log 1 "Performing Database Backup for Branch: $1, App: $APPNAME."
-  log 2 "App Name: ${APPNAME}."
-  log 2 "Date Timestamp: ${BACKUP_TIMESTAMP}."
-  log 2 "Latest Django Migration ID: ${LATEST_MIGRATION}."
-  log 2 "S3 File URL: ${S3_BUCKET_FILE_URL}"
-  log 2 "Beginning database backup process."
-
-  aws s3 cp $LOCAL_DUMP_FILENAME $S3_BUCKET_FILE_URL
+  print_header "Skipping this to get a deploy up"
+#  print_header "Creating Database Backup in S3"
+#
+#  # Create a new backup
+#  heroku pg:backups:capture -a $APPNAME
+#
+#  # Download the new backup
+#  LOCAL_DUMP_FILENAME=$APPNAME.dump
+#  heroku pg:backups:download -a $APPNAME -o $LOCAL_DUMP_FILENAME
+#
+#  # upload to s3
+#  DB_CONNECTION_STRING=$(heroku config:get DATABASE_URL -a $APPNAME);
+#  LATEST_MIGRATION=$(psql $DB_CONNECTION_STRING -qtA -c 'select name from django_migrations order by id desc limit 1;')
+#  BACKUP_TIMESTAMP=$(date '+%Y-%m-%d--%H-%M-%S')
+#  S3_DUMP_FILENAME="${APPNAME}.${BACKUP_TIMESTAMP}.${SHA}.${LATEST_MIGRATION}.dump"
+#  S3_BUCKET_FILE_URL="s3://joplin-austin-gov-archive/deployment-backups/database/${APPNAME}/${S3_DUMP_FILENAME}"
+#
+#  log 1 "Performing Database Backup for Branch: $1, App: $APPNAME."
+#  log 2 "App Name: ${APPNAME}."
+#  log 2 "Date Timestamp: ${BACKUP_TIMESTAMP}."
+#  log 2 "Latest Django Migration ID: ${LATEST_MIGRATION}."
+#  log 2 "S3 File URL: ${S3_BUCKET_FILE_URL}"
+#  log 2 "Beginning database backup process."
+#
+#  aws s3 cp $LOCAL_DUMP_FILENAME $S3_BUCKET_FILE_URL
 fi
