d14f59839add4f3657f2ec0350a0e94b1f176c13
Brian Smith <briaguya@gmail.com>
2020-04-22 12:01:44 -0500

    no map blocks

--- joplin/api/schema.py
+++ joplin/api/schema.py
@@ -95,11 +95,6 @@ def expand_by_type(key, value):
 
 
 def try_get_api_representation(StreamChild):
-    # if StreamChild.block_type == "map_block":
-    #     # this has its own representation definition in blocks.py
-    #     block = StreamChild.block.get_api_representation(StreamChild.value)
-    #     return block
-
     try:
         if not isinstance(StreamChild, dict):
             block = StreamChild.block.get_api_representation(StreamChild.value) or None
