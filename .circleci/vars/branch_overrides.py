38a1f5de525c123fe7c885892cad55ce9147527f
Chia Berry <chia.berry@austintexas.gov>
2020-04-24 08:58:43 -0500

    vars

--- .circleci/vars/branch_overrides.py
+++ .circleci/vars/branch_overrides.py
@@ -9,7 +9,7 @@
     branch_overrides are not required for every branch.
 '''
 branch_overrides = {
-    "4302-janis-preview": {
+    "4214-add-to-events": {
         "LOAD_DATA": "fixtures",
         "V3_WIP": True,
     },
