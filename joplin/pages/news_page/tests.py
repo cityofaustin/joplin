953de903691ba4bc109fa428594710857e0d2fde
Brian Smith <briaguya@gmail.com>
2020-05-07 19:17:24 -0500

    get the rest of the tests in, let's see where they fail

--- joplin/pages/news_page/tests.py
+++ joplin/pages/news_page/tests.py
@@ -36,30 +36,53 @@ def test_written_by_APH():
 
 @pytest.mark.django_db
 def test_written_by_APH_written_for_APH():
-    # APH makes a page for APH, picks APH
+    # APH makes a page for APH, doesn’t assign a different department:
     page = fixtures.written_by_APH_written_for_APH()
 
-    #   url is APH
-    #   From is APH
-    #   Written by is not shown
+    # Make sure we made a news page
     assert isinstance(page, NewsPage)
-    assert False
+
+    # Make sure we only have one instance
+    assert len(page.janis_instances()) == 1
+    janis_instance = page.janis_instances()[0]
+
+    # Make sure we only have one URL
+    assert len(page.janis_urls()) == 1
+    janis_url = page.janis_urls()[0]
+
+    # Make sure the URL is APH
+    assert janis_instance['url'] == '/mvp-news-aph/mvp-news/'
+    assert janis_url == '/mvp-news-aph/mvp-news/'
+
+    # Make sure "From" is APH
+    assert janis_instance['from'].title == 'Austin Public Health'
+
+    # Make sure written by is not shown
+    assert not janis_instance['by']
 
 
 @pytest.mark.django_db
 def test_written_by_CPIO_written_for_APH():
-    # CPIO makes a page for APH, picks APH
-    page = fixtures.written_by_CPIO_written_for_APH()
+    # APH makes a page for APH, doesn’t assign a different department:
+    page = fixtures.written_by_APH_written_for_APH()
 
-
-
-
-
-
-
-
-    #   url is APH
-    #   From is APH
-    #   Written by is CPIO
+    # Make sure we made a news page
     assert isinstance(page, NewsPage)
-    assert False
+
+    # Make sure we only have one instance
+    assert len(page.janis_instances()) == 1
+    janis_instance = page.janis_instances()[0]
+
+    # Make sure we only have one URL
+    assert len(page.janis_urls()) == 1
+    janis_url = page.janis_urls()[0]
+
+    # Make sure the URL is APH
+    assert janis_instance['url'] == '/mvp-news-aph/mvp-news/'
+    assert janis_url == '/mvp-news-aph/mvp-news/'
+
+    # Make sure "From" is APH
+    assert janis_instance['from'].title == 'Austin Public Health'
+
+    # Make sure written by is CPIO
+    assert not janis_instance['by'].title == 'Communications and Public Information Office'
