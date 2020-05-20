95925815da8e6fa83d2e443f2f268492a860e13e
Chia Berry <chia.berry@austintexas.gov>
2020-05-19 21:42:45 -0500

    remove the request

--- joplin/users/fixtures/helpers/create_request.py
+++
@@ -1,6 +0,0 @@
-# from django.test import RequestFactory
-#
-#
-# # Skips creating fixture if User with email already exists
-# def create_post_request(path, ):
-#     request = RequestFactory.post()
