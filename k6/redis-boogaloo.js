721f6becf5a332fd0cb13b3b58b6a2cb77b655d8
Brian Smith <briaguya@gmail.com>
2020-05-25 18:50:11 -0500

    add k6 scripts

---
+++ k6/redis-boogaloo.js
@@ -0,0 +1,60 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { check } from 'k6';
+export let options = {
+  vus: 10,
+  duration: '100s',
+};
+
+let query = `
+{
+  allPages {
+    edges {
+      node {
+        janisbasepagewithtopics {
+          officialdocumentpage {
+            documents {
+              edges {
+                node {
+                  title
+                  id
+                  document {
+                    filename
+                    fileSize
+                  }
+                  documentEs {
+                    filename
+                    fileSize
+                    fileHash
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+let headers = {
+  'Content-Type': 'application/json',
+};
+
+export default function() {
+  let res = http.post(
+    'https://joplin-pr-redis-boogaloo.herokuapp.com/api/graphql',
+    JSON.stringify({ query: query }),
+    { headers: headers }
+  );
+
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'body size is 261559 bytes': (r) => r.body.length == 261559,
+  });
+  // console.log(res.status);
+
+  // if (res.status === 200) {
+  //   console.log(JSON.stringify(res.body));
+  // }
+}
---
+++ k6/try-grapple-again.js
@@ -0,0 +1,45 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export let options = {
+  vus: 10,
+  duration: '30s',
+};
+
+let query = `
+{
+  pages {
+    ...on OfficialDocumentPage {
+      documents {
+        id
+        name
+        date
+        title
+        authoringOffice
+        summary
+        name
+        document {
+          file
+          fileSize
+          fileHash
+        }
+      }
+    }
+  } 
+}`;
+
+let headers = {
+  'Content-Type': 'application/json',
+};
+
+export default function() {
+  let res = http.post(
+    'https://joplin-pr-try-grapple-again.herokuapp.com/graphql',
+    JSON.stringify({ query: query }),
+    { headers: headers }
+  );
+
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'body size is 314968 bytes': (r) => r.body.length == 314968,
+  });
+}
