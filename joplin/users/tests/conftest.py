40d4e1acfb44c4a6c62c66b5f965d30cda1e3afa
Chia Berry <chia.berry@austintexas.gov>
2020-06-02 11:16:17 -0500

    remove unecessary fixtures

--- joplin/users/tests/conftest.py
+++ joplin/users/tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import groups.fixtures as department_fixtures
 import users.fixtures as user_fixtures
-import pages.service_page.fixtures as service_fixtures
 
 
 @pytest.fixture()
@@ -17,13 +16,3 @@ def superadmin():
 @pytest.fixture()
 def editor():
     return user_fixtures.editor_for_test_env()
-
-
-@pytest.fixture()
-def kitchen_service():
-    return service_fixtures.kitchen_sink()
-
-
-@pytest.fixture()
-def departmentless_service():
-    return service_fixtures.step_with_1_location()
--- joplin/users/tests/tests.py
+++ joplin/users/tests/tests.py
@@ -5,6 +5,7 @@ from base.views.new_page_from_modal import new_page_from_modal
 from pages.information_page.models import InformationPage
 from pages.home_page.factories import HomePageFactory
 from base.views.joplin_search_views import dept_explorable_pages
+import pages.service_page.fixtures as service_fixtures
 import json
 
 
@@ -201,7 +202,9 @@ def test_admin_can_make_departmentless_page(superadmin, rf):
 
 
 @pytest.mark.django_db
-def test_editor_cannot_explore_other_dept_pages(editor, kitchen_service, departmentless_service):
+def test_editor_cannot_explore_other_dept_pages(editor):
+    kitchen_service = service_fixtures.kitchen_sink()
+    departmentless_service = service_fixtures.step_with_1_location()
     explorable_pages = dept_explorable_pages(editor)
     # assert that the service page under kitchen sink department is in the results of the kitchen sink editor
     assert explorable_pages.filter(id=kitchen_service.id).exists()
@@ -210,7 +213,9 @@ def test_editor_cannot_explore_other_dept_pages(editor, kitchen_service, departm
 
 
 @pytest.mark.django_db
-def test_superadmin_explores_all_pages(superadmin, kitchen_service, departmentless_service):
+def test_superadmin_explores_all_pages(superadmin):
+    kitchen_service = service_fixtures.kitchen_sink()
+    departmentless_service = service_fixtures.step_with_1_location()
     explorable_pages = dept_explorable_pages(superadmin)
     # assert superadmins can explore all pages, regardless of department
     assert explorable_pages.filter(id=kitchen_service.id).exists()
