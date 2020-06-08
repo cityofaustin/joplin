b0edce53460307ad85e963b1a0980b7181a5f994
Chia Berry <chia.berry@austintexas.gov>
2020-06-08 13:01:52 -0500

    add permissions individually

--- joplin/groups/fixtures/helpers/group_permissions.py
+++ joplin/groups/fixtures/helpers/group_permissions.py
@@ -2,33 +2,27 @@ from django.contrib.auth.models import Permission
 
 
 def add_moderator_permissions(group):
-    perms_to_add = []
-    perms_to_add.append(Permission.objects.get(codename="view_extra_panels"))
-    perms_to_add.append(Permission.objects.get(codename="add_contact"))
-    perms_to_add.append(Permission.objects.get(codename="change_contact"))
-    perms_to_add.append(Permission.objects.get(codename="view_contact"))
-    perms_to_add.append(Permission.objects.get(codename="view_snippets"))
-    perms_to_add.append(Permission.objects.get(codename="view_user"))
-    perms_to_add.append(Permission.objects.get(codename="access_admin"))
-    perms_to_add.append(Permission.objects.get(codename="add_document"))
-    perms_to_add.append(Permission.objects.get(codename="change_document"))
-    perms_to_add.append(Permission.objects.get(codename="delete_document"))
-    perms_to_add.append(Permission.objects.get(codename="add_image"))
-    perms_to_add.append(Permission.objects.get(codename="change_image"))
-    perms_to_add.append(Permission.objects.get(codename="delete_image"))
-
-    group.permissions.add(perms_to_add)
+    group.permissions.add(Permission.objects.get(codename="view_extra_panels"))
+    group.permissions.add(Permission.objects.get(codename="add_contact"))
+    group.permissions.add(Permission.objects.get(codename="change_contact"))
+    group.permissions.add(Permission.objects.get(codename="view_contact"))
+    group.permissions.add(Permission.objects.get(codename="view_snippets"))
+    group.permissions.add(Permission.objects.get(codename="view_user"))
+    group.permissions.add(Permission.objects.get(codename="access_admin"))
+    group.permissions.add(Permission.objects.get(codename="add_document"))
+    group.permissions.add(Permission.objects.get(codename="change_document"))
+    group.permissions.add(Permission.objects.get(codename="delete_document"))
+    group.permissions.add(Permission.objects.get(codename="add_image"))
+    group.permissions.add(Permission.objects.get(codename="change_image"))
+    group.permissions.add(Permission.objects.get(codename="delete_image"))
 
 
 def add_editor_permissions(group):
-    perms_to_add = []
-    perms_to_add.append(Permission.objects.get(codename="view_user"))
-    perms_to_add.append(Permission.objects.get(codename="access_admin"))
-    perms_to_add.append(Permission.objects.get(codename="add_document"))
-    perms_to_add.append(Permission.objects.get(codename="change_document"))
-    perms_to_add.append(Permission.objects.get(codename="delete_document"))
-    perms_to_add.append(Permission.objects.get(codename="add_image"))
-    perms_to_add.append(Permission.objects.get(codename="change_image"))
-    perms_to_add.append(Permission.objects.get(codename="delete_image"))
-
-    group.permissions.add(perms_to_add)
+    group.permissions.add(Permission.objects.get(codename="view_user"))
+    group.permissions.add(Permission.objects.get(codename="access_admin"))
+    group.permissions.add(Permission.objects.get(codename="add_document"))
+    group.permissions.add(Permission.objects.get(codename="change_document"))
+    group.permissions.add(Permission.objects.get(codename="delete_document"))
+    group.permissions.add(Permission.objects.get(codename="add_image"))
+    group.permissions.add(Permission.objects.get(codename="change_image"))
+    group.permissions.add(Permission.objects.get(codename="delete_image"))
