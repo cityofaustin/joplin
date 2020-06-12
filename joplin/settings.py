a1c48ca39fc3aefad39e4981138ce59d95cf7b35
Niicck <niicck@users.noreply.github.com>
2020-06-12 15:12:50 -0500

    fix typo

--- joplin/settings.py
+++ joplin/settings.py
@@ -512,7 +512,7 @@ LOGGING = {
 }
 # Send email notifications of errors for PR build
 # TODO: this can be managed in a cleaner way
-if os.getenv("CIRCLE_BRANCH") == "4356-queue"
+if os.getenv("CIRCLE_BRANCH") == "4356-queue":
     ADMINS = [('Nick', 'nick.ivons@austintexas.gov')]
 
 # Temporary variables to toggle features for v3 while its still in development
