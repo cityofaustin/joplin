e53e922147811c88b8d817ca523d1990bb3c5a0c
Chia Berry <chia.berry@austintexas.gov>
2020-07-03 17:25:26 -0500

    add the official document collection to the content type map

--- joplin/api/content_type_map.py
+++ joplin/api/content_type_map.py
@@ -8,6 +8,7 @@ from pages.event_page.models import EventPage
 from pages.topic_page.models import TopicPage
 from pages.topic_collection_page.models import TopicCollectionPage
 from pages.department_page.models import DepartmentPage
+from pages.official_documents_collection.models import OfficialDocumentCollection
 
 # Gain access to a content_type's node and model if you have it's name.
 # Helps reduce copypasta in api.schema.py
@@ -51,5 +52,9 @@ content_type_map = {
     "department page": {
         "node": "DepartmentPageNode",
         "model": DepartmentPage,
+    },
+    "official document collection": {
+        "node": "OfficialDocumentCollection",
+        "model": OfficialDocumentCollection,
     }
 }
