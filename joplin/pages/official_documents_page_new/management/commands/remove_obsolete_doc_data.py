e2cf75beb6184b09553206f97148428240acc2c6
Chia Berry <chia.berry@austintexas.gov>
2020-07-07 09:54:54 -0500

    add function to delete obsolete data

--- joplin/pages/official_documents_page_new/management/commands/remove_obsolete_doc_data.py
+++ joplin/pages/official_documents_page_new/management/commands/remove_obsolete_doc_data.py
@@ -6,7 +6,7 @@ from pages.official_documents_page.models import OfficialDocumentPage
 def remove_obsolete_doc_data():
     OfficialDocumentPageDocument.objects.all().delete()
     OfficialDocumentPage.objects.all().delete()
-    
+
 
 class Command(BaseCommand):
     help = "Removes obsolete document data, after copy new official document pages has been run"
