dffcbf672992c6ad920232c2287dcab7c293d9da
Chia Berry <chia.berry@austintexas.gov>
2020-08-24 17:15:24 -0500

    dont forget the package

--- joplin/pages/official_documents_page/management/commands/extract_document_text.py
+++ joplin/pages/official_documents_page/management/commands/extract_document_text.py
@@ -1,6 +1,21 @@
-from urllib3.request import RequestMethods
+import urllib3
+import io
+import fitz
 from django.core.management.base import BaseCommand, CommandError
 from pages.official_documents_page.models import OfficialDocumentPage
+
+
+def extract_text_from_url(url):
+    # url = 'https://joplin3-austin-gov-static.s3.amazonaws.com/production/media/documents/2020-0547.pdf'
+    http = urllib3.PoolManager()
+    resp = http.request('GET', url)
+    file_stream = io.BytesIO(resp.data)
+    # Document('', < memory, doc  # 3>)
+    pdf_doc = fitz.open(stream=file_stream, filetype='pdf')
+    text = ''
+    for page in pdf_doc:
+        text += page.getText()
+    return text
 
 
 def extract_document_text():
@@ -18,7 +33,9 @@ def extract_document_text():
         
         '''
         doc_url = url_base + page.document.url
-        print(page)
+        extracted_text = extract_text_from_url(doc_url)
+        page.body = extracted_text
+        page.save()
 
 
 class Command(BaseCommand):
