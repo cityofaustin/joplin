b2cee6fc6ae280c6985cb7544086bc28c3c70e79
Chia Berry <chia.berry@austintexas.gov>
2020-09-02 10:10:24 -0500

    docstring and clean up

--- joplin/pages/official_documents_page/management/commands/extract_document_text.py
+++ joplin/pages/official_documents_page/management/commands/extract_document_text.py
@@ -6,31 +6,29 @@ from pages.official_documents_page.models import OfficialDocumentPage
 
 
 def extract_text_from_url(url):
+    """
+    :param url: string, address of pdf
+    :return: string with contents extracted from pdf located at url
+    """
     http = urllib3.PoolManager()
     resp = http.request('GET', url)
     file_stream = io.BytesIO(resp.data)
     text = ''
-    try:
-        pdf_doc = fitz.open(stream=file_stream, filetype='pdf')
-        for page in pdf_doc:
-            text += page.getText()
-    except RuntimeError:
-        print(f'Runtime Error for {url}')
+    pdf_doc = fitz.open(stream=file_stream, filetype='pdf')
+    for page in pdf_doc:
+        text += page.getText()
     return text
 
 
 def extract_document_text():
+    """
+    Iterates through Official Document Pages
+    if body is empty, retrieve document at url and save text in the body
+    """
     all_document_pages = OfficialDocumentPage.objects.all()
     print(f'Beginning extraction...')
 
-    url_base = 'https://joplin3-austin-gov-static.s3.amazonaws.com/production'
-
     for page in all_document_pages:
-        '''
-        get the document url
-        get the data from the document url
-        set body as text and save
-        '''
         print(f'Page id {page.id}')
         # Check if page body already has content, if so skip
         if len(page.body) > 0:
@@ -38,12 +36,11 @@ def extract_document_text():
         if page.document and page.document.url:
             filename = page.document.url.split('docs')[1]
             print(f'reading {filename}')
-            doc_url = url_base + filename
-            extracted_text = extract_text_from_url(doc_url)
+            extracted_text = extract_text_from_url(page.document.url)
             page.body = extracted_text
             page.save()
         else:
-            print(f'Official Document Page with id {page.id} url does not exist')
+            print(f'Official Document Page with id {page.id} does not have a document')
 
 
 class Command(BaseCommand):
