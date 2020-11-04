1e5940898bc7ebf8eb1962b46bd60345c7a87864
Chia Berry <chia.berry@austintexas.gov>
2020-11-04 13:04:00 -0600

    add images to rtf

--- joplin/base/models/constants.py
+++ joplin/base/models/constants.py
@@ -1,3 +1,4 @@
 WYSIWYG_GENERAL = ['h2', 'h3', 'h4', 'bold', 'link', 'ul', 'ol', 'code', 'embed', 'document-link']
+WYSIWYG_GENERAL_IMAGE = ['h2', 'h3', 'h4', 'bold', 'link', 'ul', 'ol', 'code', 'embed', 'document-link', 'image']
 SHORT_DESCRIPTION_LENGTH = 300
 DEFAULT_MAX_LENGTH = 255
